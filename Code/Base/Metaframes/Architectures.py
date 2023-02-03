354ee41c8d62b78a36caedf6756a8a7e415a306c
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-03 23:42:05 +0800

    comments for enc dec

--- Code/Base/Metaframes/Architectures.py
+++ Code/Base/Metaframes/Architectures.py
@@ -18,8 +18,13 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     """The base decoder interface for the encoder-decoder architecture.
 
-    inputs: target input data batch, state extracted from enc_outputs bt init_state method
+    inputs: 
+        target input data batch
+        state from init_state method
 
+    methods:
+        init_state: enc_outputs as input, return state
+    
     return: denoted as dec_outputs
     """
     def __init__(self):
@@ -29,6 +34,28 @@ class Decoder(nn.Module):
         raise NotImplementedError
 
     def forward(self, X, state):
+        raise NotImplementedError
+
+########## Attention-Customized decoder ##########
+class AttentionDecoder(Decoder):
+    """The base attention-based decoder interface.
+    inputs: 
+        1. target input data batch
+        2. state from init_state method
+
+    methods:
+        init_state: enc_outputs as input, return state
+    
+    attributes:
+        attention_weights
+
+    return: denoted as dec_outputs
+    """
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def attention_weights(self):
         raise NotImplementedError
 
 class EncoderDecoder(nn.Module):
@@ -54,26 +81,3 @@ class EncoderDecoder(nn.Module):
         enc_all_outputs = self.encoder(enc_X, *args)
         dec_state = self.decoder.init_state(enc_all_outputs, *args)
         return self.decoder(dec_X, dec_state)
-
-########## Attention-Customized decoder ##########
-class AttentionDecoder(Decoder):
-    """The base attention-based decoder interface.
-    args: encoder, decoder
-
-    inputs: enc_X, dec_X, others(optional)
-        encoder(enc_X, others) --> enc_outputs
-        extract enc_outputs --> state
-        decoder(dec_X, state) --> dec_outputs
-    
-    returns: denoted as dec_outputs, y_hat is included
-
-    attributes: attention_weights
-    
-    explains: be careful to design proper work for infer and train mode.
-    """
-    def __init__(self):
-        super().__init__()
-
-    @property
-    def attention_weights(self):
-        raise NotImplementedError
