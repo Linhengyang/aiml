b96556c3fd6bb3164dc405e8361e58d280005845
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-05 02:30:27 +0800

    init transformer decoder block

--- Code/Modules/_transformer.py
+++ Code/Modules/_transformer.py
@@ -42,6 +42,51 @@ class TransformerEncoderBlock(nn.Module):
     
     explains:
         keep batch shape at every layer's input/output through the block
+        encode source sequence time 1 to T directly to deep sequence time 1 to T, that is:
+            f(time 1 to T) --> node 1 to T on next layer
+    '''
+    def __init__(self, num_heads, num_hiddens, dropout, ffn_num_hiddens, use_bias=False):
+        super().__init__()
+        self.attention = MultiHeadAttention(num_heads, num_hiddens, dropout, use_bias)
+        self.addlnorm1 = AddLNorm(num_hiddens, dropout)
+        self.PosFFN = PositionWiseFFN(ffn_num_hiddens, num_hiddens)
+        self.addlnorm2 = AddLNorm(num_hiddens, dropout)
+    
+    def forward(self, X, valid_lens):
+        self_att_X = self.attention(X, X, X, valid_lens)
+        Y = self.addlnorm1(X, self_att_X)
+        posffn_Y = self.PosFFN(Y)
+        return self.addlnorm2(Y, posffn_Y)
+
+class TransformerDecoderBlock(nn.Module):
+    '''
+    components:
+        1. masked multihead attention(self-att but recursively)
+        2. addLnorm
+        3. encoder-decoder attention
+        4. addLnorm
+        5. positionwiseFFN
+        6. addLnorm
+    
+    inputs: dec_X, state
+        dec_X's shape: (batch_size, seq_len, d_dim)
+    
+    returns: denoted as dec_O
+        dec_O's shape: (batch_size, seq_len, d_dim), the same as dec_X
+    
+    explains:
+        keep batch shape at every layer's input/output through the block
+        encode target sequence time 0 to T-1 recursively to deep sequence time 1 to T, that is:
+        Infer steps:
+            f(0) --> node 1 on next layer, 1_hat
+            f(0, 1_hat) --> node 2 on next layer, 2_hat
+            ..
+            f(0, T-1_hat) --> node T on next layer, T_hat
+        Train steps:
+            f(0) --> node 1 on next layer, 1_hat
+            f(0, 1) --> node 2 on next layer, 2_hat
+            ..
+            f(0, T-1) --> node T on next layer, T_hat
     '''
     def __init__(self, num_heads, num_hiddens, dropout, ffn_num_hiddens, use_bias=False):
         super().__init__()
