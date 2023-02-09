811a25513089f053248316d5b5e2a0d660144e9e
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-09 22:56:29 +0800

    transformer test passed

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import os
+import warnings
+warnings.filterwarnings("ignore")
 import torch
 from Config import base_data_dpath, seq2seq_dname, eng2fra_fname
-# import Code.projs.transformer.DataLoad_seq2seq as test1
-import Code.Modules._transformer as test2
+import Code.projs.transformer.DataLoad_seq2seq as test1
+# import Code.Modules._transformer as test2
+import Code.projs.transformer.network as test3
 
 ## net tester
 # 拿到类定义, 手动net_args定义, 手动input_batch_shape定义
@@ -15,52 +18,45 @@ import Code.Modules._transformer as test2
 # 检测batch是否满足条件
 
 if __name__ == "__main__":
-    ### test1
-    # eng2fra = os.path.join(base_data_dpath, seq2seq_dname, eng2fra_fname)
-    # train_iter, src_vocab, tgt_vocab = test1.data_loader_seq2seq(path=eng2fra, batch_size=2, num_steps=8, num_examples=600)
-    # for X, X_valid_len, Y, Y_valid_len in train_iter:
-    #     print('X:', X)
-    #     print('valid lengths for X:', X_valid_len)
-    #     print('Y:', Y)
-    #     print('valid lengths for Y:', Y_valid_len)
-    #     print(X.dtype, Y.dtype)
-    #     break
-    ### test2
-    # ## 测试train
-    # batch_size, num_steps, d_dim = 2, 3, 6
-    # test_input_shape = (batch_size, num_steps, d_dim)
-    # test_src_X = torch.ones(test_input_shape)
-    # test_src_valid_lens = torch.tensor([1, 2])
-    # test_tgt_X = torch.ones(test_input_shape)
-    # # 设定模型参数
-    # num_heads, num_hiddens, dropout, use_bias, ffn_num_hiddens = 2, 6, 0.1, False, 8
-    # test_args = {"num_heads":num_heads, "num_hiddens":num_hiddens, "dropout":dropout,
-    #              "use_bias":use_bias, "ffn_num_hiddens":ffn_num_hiddens}
-    # # encode
-    # test_enc = test2.TransformerEncoderBlock(**test_args)
-    # enc_info = (test_enc(test_src_X, test_src_valid_lens), test_src_valid_lens)
-    # # decode
-    # test_dec = test2.TransformerDecoderBlock(blk_ind=0, **test_args)
-    # dec_output = test_dec(test_tgt_X, enc_info)
-    # print(dec_output[0].shape)
-    # print(dec_output[1])
+    ### test3
+    eng2fra = os.path.join(base_data_dpath, seq2seq_dname, eng2fra_fname)
+    train_iter, src_vocab, tgt_vocab = test1.data_loader_seq2seq(path=eng2fra, batch_size=2, num_steps=8, num_examples=600)
+    for X, X_valid_len, Y, Y_valid_len in train_iter:
+        print('X:', X)
+        print('valid lengths for X:', X_valid_len)
+        print('Y:', Y)
+        print('valid lengths for Y:', Y_valid_len)
+        print(X.dtype, Y.dtype)
+        print("**********")
+        break
+    num_blk, num_heads, num_hiddens, dropout, use_bias, ffn_num_hiddens = 3, 2, 6, 0.1, False, 8
+    test_args = {"num_heads":num_heads, "num_hiddens":num_hiddens, "dropout":dropout,
+                 "use_bias":use_bias, "ffn_num_hiddens":ffn_num_hiddens, "num_blk":num_blk}
+    transenc = test3.TransformerEncoder(vocab_size=len(src_vocab), **test_args)
+    transdec = test3.TransformerDecoder(vocab_size=len(tgt_vocab), **test_args)
+    net = test3.Transformer(transenc, transdec)
+    print('********* test train transformer **********')
+    dec_outputs = net(X, Y, X_valid_len)
+    print(dec_outputs[0].shape == torch.Size([2, 8, len(tgt_vocab)]))
+    print(dec_outputs[1] is None)
+    print('********* test infer transformer **********')
+    net.eval()
+    eng = "i lost ."
+    src_X = torch.tensor(src_vocab[eng.split()] + [src_vocab['<eos>']]).unsqueeze(0)
+    print(eng, " now is ", src_X)
+    src_valid_len = torch.tensor([src_X.shape[1], ])
+    print(eng, " valid length is ", src_valid_len)
+    enc_outputs = net.encoder(src_X, src_valid_len)
 
-    # ## 测试infer
-    # batch_size, num_steps, d_dim = 1, 3, 6
-    # test_input_shape = (batch_size, num_steps, d_dim)
-    # test_src_X = torch.ones(test_input_shape)
-    # test_src_valid_lens = torch.tensor([num_steps,])
-    # test_tgt_X = torch.ones((1, 1, 6))
-    # # 设定模型参数
-    # num_heads, num_hiddens, dropout, use_bias, ffn_num_hiddens = 2, 6, 0.1, False, 8
-    # test_args = {"num_heads":num_heads, "num_hiddens":num_hiddens, "dropout":dropout,
-    #              "use_bias":use_bias, "ffn_num_hiddens":ffn_num_hiddens}
-    # # encode
-    # test_enc = test2.TransformerEncoderBlock(**test_args)
-    # enc_info = (test_enc(test_src_X, test_src_valid_lens), test_src_valid_lens)
-    # # decode
-    # test_dec = test2.TransformerDecoderBlock(blk_ind=0, **test_args)
-    # test_dec.eval()
-    # dec_output = test_dec(test_tgt_X, enc_info, {})
-    # print(dec_output[0].shape)
-    # print(dec_output[1])
+    enc_info = net.decoder.init_state(enc_outputs)
+    infer_recorder = {}
+    num_preds = 3
+    tgt_X = torch.tensor( [tgt_vocab['<bos>'],] ).unsqueeze(0)
+    print('initial tgt token is ', tgt_X)
+    print('initial infer recorder is ', infer_recorder)
+    for i in range(num_preds):
+        X_hat, infer_recorder = net.decoder(tgt_X, enc_info, infer_recorder)
+        print('infer recorder at step ', i, ' is ', infer_recorder)
+        print('X_hat shape is ', X_hat.shape)
+        tgt_X = X_hat.argmax(dim=-1)
+        print('tgt token at step ', i, ' is ', tgt_vocab.to_tokens(tgt_X.item()))
