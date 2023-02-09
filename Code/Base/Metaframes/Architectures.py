aec348f5ef11e3e3480c6c552bec5bd702499d05
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-09 21:54:28 +0800

    interface for encoder-decoder updated

--- Code/Base/Metaframes/Architectures.py
+++ Code/Base/Metaframes/Architectures.py
@@ -74,10 +74,6 @@ class EncoderDecoder(nn.Module):
     """
     def __init__(self, encoder, decoder):
         super().__init__()
-        self.encoder = encoder
-        self.decoder = decoder
 
     def forward(self, enc_X, dec_X, *args):
-        enc_all_outputs = self.encoder(enc_X, *args)
-        dec_state = self.decoder.init_state(enc_all_outputs, *args)
-        return self.decoder(dec_X, dec_state)
+        raise NotImplementedError
