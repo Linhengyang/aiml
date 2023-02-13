83923ecf7a7250f82090d7c1636c83ea19ea9f84
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-14 02:17:55 +0800

    test for transformer trainer implemented. unexecuted

--- test.py
+++ test.py
@@ -2,29 +2,43 @@ import os
 import warnings
 warnings.filterwarnings("ignore")
 import torch
-from Config import base_data_dpath, seq2seq_dname, eng2fra_fname, eng2fra_test_fname
+from Config import base_data_dpath, seq2seq_dname, eng2fra_train_fname, eng2fra_valid_fname, eng2fra_test_fname
 from Code.projs.transformer.DataLoad_seq2seq import data_loader_seq2seq
 from Code.projs.transformer.Trainer import transformerTrainer
 from Code.projs.transformer.Network import TransformerEncoder, TransformerDecoder, Transformer
+from Code.Loss.MaskedCELoss import MaskedSoftmaxCELoss
 
 if __name__ == "__main__":
-    eng2fra = os.path.join(base_data_dpath, seq2seq_dname, eng2fra_fname)
+    eng2fra_train = os.path.join(base_data_dpath, seq2seq_dname, eng2fra_train_fname)
+    eng2fra_valid = os.path.join(base_data_dpath, seq2seq_dname, eng2fra_valid_fname)
     eng2fra_test = os.path.join(base_data_dpath, seq2seq_dname, eng2fra_test_fname)
-    train_iter, src_vocab, tgt_vocab = data_loader_seq2seq(path=eng2fra, batch_size=2, num_steps=8, num_examples=600)
-    init_iter, _, _ = data_loader_seq2seq(path=eng2fra_test, batch_size=2, num_steps=8)
-
+    train_iter, src_vocab, tgt_vocab = data_loader_seq2seq(path=eng2fra_train, batch_size=2, num_steps=8)
+    valid_iter, _, _ = data_loader_seq2seq(path=eng2fra_valid, batch_size=2, num_steps=8)
+    test_iter, _, _ = data_loader_seq2seq(path=eng2fra_test, batch_size=2, num_steps=8, num_examples=3)
+    # design net & loss
     num_blk, num_heads, num_hiddens, dropout, use_bias, ffn_num_hiddens = 2, 2, 6, 0.1, False, 8
     test_args = {"num_heads":num_heads, "num_hiddens":num_hiddens, "dropout":dropout,
                  "use_bias":use_bias, "ffn_num_hiddens":ffn_num_hiddens, "num_blk":num_blk}
     transenc = TransformerEncoder(vocab_size=len(src_vocab), **test_args)
     transdec = TransformerDecoder(vocab_size=len(tgt_vocab), **test_args)
     net = Transformer(transenc, transdec)
-
-    trainer = transformerTrainer(net, None, train_iter, 3, None, 
-                                 None, None, init_iter)
-    # trainer.topo_logger('lazy_topo.txt')
-    trainer.net_resolver()
-    trainer.param_initializer()
-    # trainer.topo_logger('defined_topo.txt')
-    for X, X_valid_lens, Y, Y_valid_lens in train_iter:
-        break
+    loss = MaskedSoftmaxCELoss()
+    # init trainer
+    trainer = transformerTrainer(net=net, loss=loss, num_epochs=3)
+    ## set the data iters
+    trainer.set_data_iter(tgt_vocab, train_iter, valid_iter, test_iter)
+    ## set the device
+    trainer.set_device(torch.device('cuda'))
+    ## set the optimizer
+    trainer.set_optimizer(lr=0.005)
+    ## set the grad clipper
+    trainer.set_grad_clipping(grad_clip_val=1.0)
+    ## print the lazy topology
+    trainer.log_topology('lazy_topo.txt')
+    ## check the net & loss
+    check_flag = trainer.resolve_net(need_resolve=True)
+    if check_flag:
+        ## print the defined topology
+        trainer.log_topology('defined_topo.txt')
+        trainer.init_params()
+    trainer.fit()
