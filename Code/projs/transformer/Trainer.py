0e82302e303d21ef896454736a2c0d19fb4c9f78
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-14 00:22:33 +0800

    transformer easyTrainer API updated

--- Code/projs/transformer/Trainer.py
+++ Code/projs/transformer/Trainer.py
@@ -2,29 +2,44 @@ import os
 import torch
 from torch import nn as nn
 from .Network import Transformer
-from ...Compute import Trainer
+from ...Compute import easyTrainer
 
-class transformerTrainer(Trainer):
+class transformerTrainer(easyTrainer):
     def __init__(self, net, loss, train_data_iter, num_epochs, device, 
-                 test_data_iter=None, grad_clip_val=None, init_batch=None):
+                 test_data_iter=None, init_batch=None):
         super().__init__()
         self.net = net
         self.loss = loss
-        self.grad_clip_val = grad_clip_val
         self.device = device
         self.num_epochs = num_epochs
         self.train_data_iter = train_data_iter
         self.test_data_iter = test_data_iter
         self.init_batch = init_batch
     
-    def topo_logger(self, fname, topos_dir='/Users/lhy/studyspace/online/topos'):
+    def log_topology(self, fname, topos_dir='/Users/lhy/studyspace/online/topos'):
         '''log the topology of the network to topos directory
         file path: /Users/lhy/studyspace/online/topos/transformer/XXXXX.txt
         '''
         with open(os.path.join(topos_dir, 'transformer', fname), 'w') as f:
             print(self.net, file=f)
     
-    def net_resolver(self):
+    def set_device(self, device=None):
+        if device is not None:
+            self.device = device
+        else:
+            self.device = torch.device('cpu')
+
+    def set_data_iter(self, train_data_iter, test_data_iter=None):
+        def inner_iter(data_iter):
+            if data_iter is None:
+                return None
+            for batch in data_iter:
+                # set device
+                yield batch
+        self.train_data_iter = inner_iter(train_data_iter)
+        self.test_data_iter = inner_iter(test_data_iter)
+
+    def resolve_net(self):
         '''
         only when init_batch is a train_iter-like data generator, this function can be executed
         '''
@@ -34,19 +49,46 @@ class transformerTrainer(Trainer):
             break
         self.net(X, Y, X_valid_len)
     
-    def param_initializer(self):
+    def init_params(self):
         '''
-        customize the weights initialization behavior
+        customize the weights initialization behavior and initialize the net
         '''
         def xavier_init_weights(m):
             if type(m) == nn.Linear:
                 nn.init.xavier_uniform_(m.weight)
         self.net.apply(xavier_init_weights)
     
-    def param_optimizer(self, lr, optim_type='adam'):
+    def set_optimizer(self, lr, optim_type='adam'):
+        '''
+        set the optimizer at attribute optimizer
+        '''
         assert type(lr) == float, 'learning rate should be a float'
         if optim_type == 'adam':
             self.optimizer = torch.optim.Adam(self.net.parameters(), lr=lr)
     
-    def grad_clipper(self):
-        raise NotImplementedError
+    def set_grad_clipping(self, grad_clip_val=None):
+        '''
+        set the grad_clip_val at attribute grad_clip_val if input a float
+        '''
+        self.grad_clip_val = grad_clip_val
+
+    def fit(self):
+        assert self.optimizer is not None, 'No optimizer detected in trainer'
+        self.net.train()
+        for epoch in range(self.num_epochs):
+            for batch in self.train_data_iter:
+                self.optimizer.zero_grad()
+                ## get data and compute loss
+                X, X_valid_len, Y, Y_valid_len = batch
+                Y_hat, _ = self.net(X, Y, X_valid_len)
+                l = self.loss(Y_hat, Y, Y_valid_len)
+                ## bp/update params/evaluate model
+                l.sum().backward()
+                if self.grad_clip_val is not None:
+                    nn.utils.clip_grad_norm_(self.net.parameters(), self.grad_clip_val)
+                self.optimizer.step()
+                with torch.no_grad():
+                    self.evaluator()
+            if (epoch + 1) % 100 == 0:
+                self.visualizer()
+    
