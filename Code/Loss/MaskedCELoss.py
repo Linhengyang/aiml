3a0f810ba7fec17253a466bb5599c1dd43f51715
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-15 01:57:07 +0800

    comment about masked loss updated

--- Code/Loss/MaskedCELoss.py
+++ Code/Loss/MaskedCELoss.py
@@ -18,9 +18,7 @@ class MaskedSoftmaxCELoss(nn.CrossEntropyLoss):
            shape: (batch_size,)
     output:
         Loss tensor wight shape (batch_size,)
-        Only losses from valid area are averaged for every sample.
-        教科书这里有点问题, invalid tokens' losses 未被考虑, 但是在平均loss时却计入了这些invalid tokens,
-        导致invalid tokens越多的序列sample, 其loss越小。
+        Only losses from valid area are summed for every sample.
     '''
     def __init__(self):
         super().__init__()
@@ -31,17 +29,14 @@ class MaskedSoftmaxCELoss(nn.CrossEntropyLoss):
         label: (batch_size, num_steps)
         '''
         self.reduction = 'none'
-        # unmasked_loss shape: (batch_size, num_steps)
-        # mask_mat shape: (batch_size, num_steps)
+        # unmasked_loss shape: (batch_size, num_steps) with float elements
+        # mask_mat shape: (batch_size, num_steps) with 0-1 elements where 1 indicates valid
         unmasked_loss = super(MaskedSoftmaxCELoss, self).forward(pred.permute(0,2,1), label)
         def mask_matrix(X, valid_len):
             weights = torch.ones_like(X)
             maxlen = X.size(1)
             flags = torch.arange(maxlen, dtype=torch.float32, device=X.device).unsqueeze(0) < valid_len.unsqueeze(1)
             weights[~flags] = 0
-            # weights /= weights.sum(dim=1, keepdim=True)
-            # return weights.nan_to_num()
             return weights
         mask = mask_matrix(unmasked_loss, valid_len)
-        # return (unmasked_loss * mask).sum(dim=1)
-        return (unmasked_loss * mask).mean(dim=1)
+        return (unmasked_loss * mask).sum(dim=1)
