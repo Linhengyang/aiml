c0095054b4b306a4a41daf83f5fc533d88d58b52
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-16 00:50:02 +0800

    base_dir seq2seq dir name changed. online log & model dir add. reveal_cnt & eval_cnt for train add

--- Config/params.py
+++ Config/params.py
@@ -1,5 +1,11 @@
-base_data_dpath = '../../data'
-seq2seq_dname = 'seq2seq'
+base_data_dir = '../../data'
+seq2seq_dir = 'seq2seq'
 eng2fra_train_fname = 'eng2fra.txt'
 eng2fra_valid_fname = 'eng2fra_valid.txt'
 eng2fra_test_fname = 'eng2fra_test.txt'
+
+online_log_dir = '/Users/lhy/studyspace/online/logs'
+online_model_save_dir = '/Users/lhy/studyspace/online/model'
+
+reveal_cnt_in_train = 30
+eval_cnt_in_train = 6
