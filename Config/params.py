c8c91935f8def1f45cd3b235a445b4296b39d6c4
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-16 11:11:36 +0800

    move proj-specific params to settings under proj

--- Config/params.py
+++ Config/params.py
@@ -3,9 +3,3 @@ seq2seq_dir = 'seq2seq'
 eng2fra_train_fname = 'eng2fra.txt'
 eng2fra_valid_fname = 'eng2fra_valid.txt'
 eng2fra_test_fname = 'eng2fra_test.txt'
-
-online_log_dir = '/Users/lhy/studyspace/online/logs'
-online_model_save_dir = '/Users/lhy/studyspace/online/model'
-
-reveal_cnt_in_train = 30
-eval_cnt_in_train = 6
