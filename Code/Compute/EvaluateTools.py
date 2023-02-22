11a335e1e3726bafcb0433f8affd3c630ff186b9
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-22 16:04:57 +0800

    move accuracy to evaluate tools

--- Code/Compute/EvaluateTools.py
+++ Code/Compute/EvaluateTools.py
@@ -89,3 +89,14 @@ def bleu(pred_seq, label_seq, k):
                 label_subs[' '.join(pred_tokens[i: i + n])] -= 1
         score *= math.pow(num_matches / (len_pred - n + 1), math.pow(0.5, n))
     return score
+
+def accuracy(y_hat, y):
+    """
+    计算预测正确的数量, 类似nn.CrossEntropyLoss
+    y_hat: (batch_size, num_classes, positions(optional)), elements是logit或softmax后的Cond Prob
+    y: (batch_size, positions(optional)), elements是label(非one-hot), dtype是torch.int64
+    """
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(dim=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
