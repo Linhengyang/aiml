5a595feeece6daa50f39c17d5da4b0e811e44102
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-24 23:23:58 +0800

    update tensors2batch_iter comment

--- Code/Utils/Common/DataAssemble.py
+++ Code/Utils/Common/DataAssemble.py
@@ -1,9 +1,9 @@
 from torch.utils import data
 
-def tensors2batch_iter(data_arrays, batch_size, is_train=True):
+def tensors2batch_iter(data_tensors, batch_size, is_train=True):
     """
-    inputs: data_arrays, batch_size, is_train(optional)
-        data_arrays: container, consisted of multiple parts with same length, e.g (features, labels, indices,...)
+    inputs: data_tensors, batch_size, is_train(optional)
+        data_tensors: container, consisted of multiple tensors(torch.tensor) with same length, e.g (features, labels, indices,...). 
         batch_size: batch_size for every batch
         is_train: whether to shuffle sample. True for train mode
 
@@ -13,5 +13,5 @@ def tensors2batch_iter(data_arrays, batch_size, is_train=True):
     explains:
         组装features和labels等, 给出minibatch data loader
     """
-    dataset = data.TensorDataset(*data_arrays)
+    dataset = data.TensorDataset(*data_tensors)
     return data.DataLoader(dataset, batch_size, shuffle=is_train)
