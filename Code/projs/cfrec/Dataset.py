75d85c4ba068a42bf3ad884d38e0a0fbeb81f2bd
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-26 17:47:59 +0800

    movielens dataset add

--- Code/projs/cfrec/Dataset.py
+++ Code/projs/cfrec/Dataset.py
@@ -1,12 +1,53 @@
 import torch
+import pandas as pd
+import numpy as np
 
-class projDataset(torch.utils.data.Dataset):
-    def __init__(self):
-        pass
+def id2index(idx):
+    return int(idx) - 1
+
+def newest_mask(df):
+    df['newest_mask'] =  df['timestamp'] != df['timestamp'].max()
+    return df
+
+class MovieLensDataset(torch.utils.data.Dataset):
+    def __init__(self, path, is_train, split_method='time-aware', test_ratio = 0.1, seed=1026):
+        super().__init__()
+        names = ['user_id', 'item_id', 'rating', 'timestamp']
+        data = pd.read_csv(path, '\t', names=names, engine='python')
+        self._num_users = data.user_id.unique().shape[0]
+        self._num_items = data.item_id.unique().shape[0]
+        data['user_id'] = data['user_id'].map(id2index)
+        data['item_id'] = data['item_id'].map(id2index)
+        if split_method == 'time-aware':
+            data.sort_values(['user_id', 'timestamp'], ascending=[True, True], inplace=True)
+            data = data.groupby('user_id').apply(newest_mask)
+            mask = data.newest_mask.to_list()
+            data.drop('newest_mask', axis=1, inplace=True)
+        elif split_method == 'random':
+            np.random.seed(seed) # 固定随机seed, 以保证trainset和testset是互补的
+            mask = np.random.uniform(0, 1, len(data)) < 1 - test_ratio
+        if is_train:
+            self._split_data = data[mask]
+        else:
+            self._split_data = data[~mask]
+        self.user_tensor = torch.tensor(self._split_data.user_id.to_list())
+        self.item_tensor = torch.tensor(self._split_data.item_id.to_list())
+        self.score_tensor = torch.tensor(self._split_data.rating.to_list())
     
     def __getitem__(self, index):
-        pass
+        return (self.user_tensor[index], self.item_tensor[index], self.score_tensor[index])
     
     def __len__(self):
-        pass
+        return self._split_data.shape[0]
     
+    @property
+    def num_users(self):
+        return self._num_users
+    
+    @property
+    def num_items(self):
+        return self._num_items
+    
+    @property
+    def dataframe(self):
+        return self._split_data
