59dd44f568eeb0940ded4f079021b06db7257ef4
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-28 13:13:49 +0800

    Itembased AutoRec add

--- Code/projs/cfrec/Network.py
+++ Code/projs/cfrec/Network.py
@@ -14,3 +14,19 @@ class explicitCF(nn.Module):
     
     def __str__(self):
         return self.mf.__str__()
+
+class ItemBasedAutoRec(nn.Module):
+    def __init__(self, num_hiddens, num_users, dropout=0.05):
+        super().__init__()
+        self.encoder = nn.Linear(num_users, num_hiddens)
+        self.act = nn.Sigmoid()
+        self.decoder = nn.Linear(num_hiddens, num_users)
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, input_batch_matrix):
+        # input shape: (batch_size=num_items_batch, num_users)
+        pred = self.decoder(self.dropout(self.act(self.encoder(input_batch_matrix))))
+        if self.training:
+            return pred * torch.sign(input_batch_matrix)
+        else:
+            return pred
