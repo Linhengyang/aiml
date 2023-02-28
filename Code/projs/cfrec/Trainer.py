cd1a0fe17756ea58505329b4c9dcf98e7e4c4fbc
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-02-28 13:15:13 +0800

    autorec trainer add

--- Code/projs/cfrec/Trainer.py
+++ Code/projs/cfrec/Trainer.py
@@ -131,3 +131,64 @@ class mfTrainer(easyTrainer):
                 self.epoch_evaluator.evaluate_model(self.net, self.loss, self.valid_iter)
                 self.epoch_evaluator.epoch_metric_cast()
         print('Fitting finished successfully')
+
+class autorecTrainer(mfTrainer):
+    def __init__(self, net, loss, num_epochs, batch_size):
+        super().__init__(net, loss, num_epochs, batch_size)
+
+    def set_data_iter(self, train_set, valid_set=None, test_set=None):
+        ''' 输入train_set(必须), valid_set(可选), test_set(可选, 用于resolve网络), 依次得到train_iter, valid_iter, test_iter'''
+        assert hasattr(self, 'device'), "Device not set. Please set trainer's device before setting data_iters"
+        train_set = train_set.to(self.device)
+        self.train_iter = torch.utils.data.DataLoader(train_set, self.batch_size, True)
+        if valid_set:
+            valid_set = valid_set.to(self.device)
+            self.valid_iter = torch.utils.data.DataLoader(valid_set, self.batch_size, False)
+        if test_set:
+            test_set = test_set.to(self.device)
+            self.test_iter = torch.utils.data.DataLoader(test_set, self.batch_size, False)
+    
+    def resolve_net(self, need_resolve=False):
+        '''
+        用test_data_iter的first batch对net作一次forward计算, 使得所有lazyLayer被确定(resolve).随后检查整个前向过程和loss计算(check).
+        resolve且check无误 --> True; resolve或check有问题 --> raise AssertionError; 不resolve或check直接训练 --> False
+        '''
+        if need_resolve:
+            assert hasattr(self, 'test_iter'), 'Please input test_set when deploying .set_data_iter()'
+            self.net.train()
+            for input_batch_matrix in self.test_iter:
+                break
+            try:
+                S_hat = self.net(input_batch_matrix)
+                weight_params = [param for param_name, param in self.net.named_parameters() if param_name.endswith('weight')]
+                l = self.loss(S_hat, input_batch_matrix, *weight_params)
+                print('Net resolved & logged. Net & Loss checked. Ready to fit')
+                return True
+            except:
+                raise AssertionError('Net or Loss has problems. Please check code before fit')
+        print('Net unresolved. Net & Loss unchecked. Ready to skip init_params and fit')
+        return False
+    
+    def fit(self):
+        assert hasattr(self, 'device'), 'device is not specified'
+        assert hasattr(self, 'optimizer'), 'optimizer missing'
+        assert hasattr(self, 'train_iter'), 'data_iter missing'
+        assert hasattr(self, 'epoch_evaluator'), 'epoch_evaluator(train log file) missing'
+        for epoch in range(self.num_epochs):
+            self.net.train()
+            self.epoch_evaluator.epoch_judge(epoch)
+            for input_batch_matrix in self.train_iter:
+                self.optimizer.zero_grad()
+                S_hat = self.net(input_batch_matrix)
+                weight_params = [param for param_name, param in self.net.named_parameters() if param_name.endswith('weight')]
+                l = self.loss(S_hat, input_batch_matrix, *weight_params)
+                l.backward()
+                if hasattr(self, 'grad_clip_val') and self.grad_clip_val is not None:
+                    nn.utils.clip_grad_norm_(self.net.parameters(), self.grad_clip_val)
+                self.optimizer.step()
+                with torch.no_grad():
+                    self.epoch_evaluator.batch_record(S_hat, input_batch_matrix, l, torch.sign(input_batch_matrix).sum().item())
+            with torch.no_grad():
+                self.epoch_evaluator.evaluate_model(self.net, self.loss, self.valid_iter)
+                self.epoch_evaluator.epoch_metric_cast()
+        print('Fitting finished successfully')
