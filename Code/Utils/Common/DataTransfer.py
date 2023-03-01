1bb8c77afe719e69f4c64b7514c8f6702b2f6da9
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-03-01 21:24:18 +0800

    comments add for functions

--- Code/Utils/Common/DataTransfer.py
+++ Code/Utils/Common/DataTransfer.py
@@ -1,10 +1,11 @@
 import torch
 import numpy as np
 import torch.nn as nn
-from collections import defaultdict
 
 def onehot_concat_multifeatures(input_tensor, num_classes):
     '''
+    此函数将多个categorical features 分别onehot变换并在最后一维concat
+
     input:
     1. input_tensor:
     (*, num_features), with elements are level-index of categorical features
@@ -46,47 +47,11 @@ def onehot_concat_multifeatures(input_tensor, num_classes):
     return nn.functional.one_hot(input_tensor + offsets, num_classes.sum()).sum(dim=-2)
 
 def offset_multifeatures(input_tensor, num_classes):
+    '''
+    此函数将多个categorical features根据num_classes作offset变换. offset变换指: feat1不变, feat2 = feat2_level + feat1_size, feat3 = feat3_level + feat1&2_size
+    这样保证了不同的feat坐落在不同的独立区间内, 使得embedding的时候可以针对每个feat作独立的embedding
+    '''
     assert len(num_classes) == input_tensor.shape[-1], 'every feature must have its num_class'
     assert torch.all(input_tensor < num_classes), 'index number exceeds or be equal to num_classes. Index number must be smaller than corresponding num_class'
     offsets = torch.cat([torch.zeros(1,), torch.cumsum(num_classes, dim=0)[:-1]], dim=0).type(num_classes.dtype).to(input_tensor.device)
     return (input_tensor + offsets).type(input_tensor.dtype)
-
-class CTRDataset(torch.utils.data.Dataset):
-    def __init__(self, data_path, feat_mapper=None, defaults=None,
-                 min_threshold=4, num_feat=34):
-        self.NUM_FEATS, self.count, self.data = num_feat, 0, {}
-        feat_cnts = defaultdict(lambda: defaultdict(int))
-        self.feat_mapper, self.defaults = feat_mapper, defaults
-        self.field_dims = np.zeros(self.NUM_FEATS, dtype=np.int64)
-        with open(data_path) as f:
-            for line in f:
-                instance = {}
-                values = line.rstrip('\n').split('\t')
-                if len(values) != self.NUM_FEATS + 1:
-                    continue
-                label = np.float32([0, 0])
-                label[int(values[0])] = 1
-                instance['y'] = [np.float32(values[0])]
-                for i in range(1, self.NUM_FEATS + 1):
-                    feat_cnts[i][values[i]] += 1
-                    instance.setdefault('x', []).append(values[i])
-                self.data[self.count] = instance
-                self.count = self.count + 1
-        if self.feat_mapper is None and self.defaults is None:
-            feat_mapper = {i: {feat for feat, c in cnt.items() if c >=
-                               min_threshold} for i, cnt in feat_cnts.items()}
-            self.feat_mapper = {i: {feat_v: idx for idx, feat_v in enumerate(feat_values)}
-                                for i, feat_values in feat_mapper.items()}
-            self.defaults = {i: len(feat_values) for i, feat_values in feat_mapper.items()}
-        for i, fm in self.feat_mapper.items():
-            self.field_dims[i - 1] = len(fm) + 1
-        self.offsets = np.array((0, *np.cumsum(self.field_dims).asnumpy()
-                                 [:-1]))
-
-    def __len__(self):
-        return self.count
-
-    def __getitem__(self, idx):
-        feat = np.array([self.feat_mapper[i + 1].get(v, self.defaults[i + 1])
-                         for i, v in enumerate(self.data[idx]['x'])])
-        return feat + self.offsets, self.data[idx]['y']
