195a72ff0fc247121a224596a1e83d09e5378f45
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-03-02 02:30:51 +0800

    CategDataParser

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 warnings.filterwarnings("ignore")
 from Code.projs.recsys._jobs import mf_train_job, mf_infer_job, autorec_train_job, autorec_infer_job
-from Code.Utils.Common.DataTransfer import CategDataParser
+from Code.Utils.Common.DataTransform import CategDataParser
 if __name__ == "__main__":
     '''
     test code in this test.py file. After successful tests, code will be moved to _jobs.py under proj_name
