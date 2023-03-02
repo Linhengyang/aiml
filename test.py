eb75925e310702d6810a0be77850f89f5a6536c7
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-03-02 22:37:24 +0800

    d2lCTRdataset test passed

--- test.py
+++ test.py
@@ -4,13 +4,16 @@ import torch
 import torch.nn as nn
 warnings.filterwarnings("ignore")
 from Code.projs.recsys._jobs import mf_train_job, mf_infer_job, autorec_train_job, autorec_infer_job
-from Code.Utils.Common.DataTransform import CategDataParser
+from Code.projs.recsys.Dataset import d2lCTRDataset
+
 if __name__ == "__main__":
     '''
     test code in this test.py file. After successful tests, code will be moved to _jobs.py under proj_name
     '''
-    data_path = ['../../data/CTR/train.csv', '../../data/CTR/test.csv']
-    ctr_data_parser = CategDataParser(data_path, [1, 34])
-    data = ctr_data_parser.mapped_offset_data
-    print(data[0])
-    print(data.shape)
+    data_path = ['../../data/CTR/train.csv',]
+    trainset = d2lCTRDataset(data_path)
+    print(trainset.num_classes)
+    for X, y in torch.utils.data.DataLoader(trainset, batch_size=3, shuffle=True):
+        print(X)
+        print(y)
+        break
