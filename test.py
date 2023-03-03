750eefbb7edd2f4355ad1f2b967c7a56f2db2e69
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-03-03 22:36:36 +0800

    deepfm train job test passed

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import warnings
 import torch
 import torch.nn as nn
 warnings.filterwarnings("ignore")
-from Code.projs.recsys._jobs import mf_train_job, mf_infer_job, autorec_train_job, autorec_infer_job, fm_train_job
+from Code.projs.recsys._jobs import mf_train_job, mf_infer_job, autorec_train_job, autorec_infer_job, fm_train_job, deepfm_train_job
 
 if __name__ == "__main__":
     '''
     test code in this test.py file. After successful tests, code will be moved to _jobs.py under proj_name
     '''
-    fm_train_job()
+    deepfm_train_job()
