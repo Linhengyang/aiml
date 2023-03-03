f9ea699a56860a75498db6d7075304001907a186
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-03-03 12:34:27 +0800

    factorization machine train test passed

--- test.py
+++ test.py
@@ -3,17 +3,10 @@ import warnings
 import torch
 import torch.nn as nn
 warnings.filterwarnings("ignore")
-from Code.projs.recsys._jobs import mf_train_job, mf_infer_job, autorec_train_job, autorec_infer_job
-from Code.projs.recsys.Dataset import d2lCTRDataset
+from Code.projs.recsys._jobs import mf_train_job, mf_infer_job, autorec_train_job, autorec_infer_job, fm_train_job
 
 if __name__ == "__main__":
     '''
     test code in this test.py file. After successful tests, code will be moved to _jobs.py under proj_name
     '''
-    data_path = ['../../data/CTR/train.csv',]
-    trainset = d2lCTRDataset(data_path)
-    print(trainset.num_classes)
-    for X, y in torch.utils.data.DataLoader(trainset, batch_size=3, shuffle=True):
-        print(X)
-        print(y)
-        break
+    fm_train_job()
