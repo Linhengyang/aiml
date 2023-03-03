8bb09068e706cbb03ba01925775059e22871303f
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-03-03 12:35:04 +0800

    MultiCategFeatEmbedding moved under rootlayer

--- Code/Base/RootLayers/MultiCategFeatEmbedding.py
+++ Code/Base/RootLayers/MultiCategFeatEmbedding.py
@@ -9,7 +9,7 @@ def offset_multifeatures(input_tensor, num_classes):
     offsets = torch.cat([torch.zeros(1,), torch.cumsum(num_classes, dim=0)[:-1]], dim=0).type(num_classes.dtype)
     return (input_tensor + offsets).type(input_tensor.dtype)
 
-class MultiCategFeatEmbedding(nn.Embedding):
+class MultiCategFeatEmbedding(nn.Module):
     '''
     Embedding layer for multiple categorical features which had already been index-preprocessed.
     Compare with vanilla nn.Embedding:
@@ -21,31 +21,31 @@ class MultiCategFeatEmbedding(nn.Embedding):
     See `offset_multifeatures` function or `onehot_concat_multifeatures` function in Utils/DataTransform
 
     args:
-        num_embeddings (int): shall be sum{num_classes of all features}. If not, a warning will be printed and num_embeddings will be reset automatically
+        num_classes (Tensor): shape (num_features, ), with elements are number of levels(classes) for every categorical feature
         embedding_dim (int): the size of each feature's embedding vector
+        flatten (Optional, Bool): If True, embedding output will flatten all features' embedded tensors. Ouput last dim will be "num_features * embedding_dim"
     
     input:
         input (Tensor): shape (*, num_features), with elements are level-index of categorical features
-        num_classes (Tensor): shape (num_features, ), with elements are number of levels(classes) for every categorical feature
-        flatten (Optional, Bool): If True, embedding output will flatten all features' embedded tensors. Ouput last dim will be "num_features * embedding_dim"
-    
+
     output:
         if flatten is True:
             returned tensor with shape (*, num_features * embedding_dim)
         else:
             returned tensor with shape (*, num_features, embedding_dim)
     '''
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, num_classes: Tensor, num_factor: int, flatten: bool, *args, **kwargs):
+        super().__init__()
+        self.num_classes = num_classes
+        self.flatten = flatten
+        self.embedding = nn.Embedding(int(num_classes.sum()), num_factor, *args, **kwargs)
     
-    def forward(self, input: Tensor, num_classes: Tensor, flatten=True) -> Tensor:
-        input_ = offset_multifeatures(input, num_classes)
-        num_embeddings = int(num_classes.sum())
-        if self.num_embeddings != num_embeddings:
-            warnings.warn(f'arg num_embeddings must be the sum of number of classes of all features. num_embeddings={num_embeddings} set automatically')
-            self.num_embeddings = num_embeddings
-        embed_ = super(MultiCategFeatEmbedding, self).forward(input_)
-        if flatten:
-            return embed_.flatten(start_dim=-2)
+    def forward(self, input: Tensor):
+        # input shape: (*, num_features)
+        input_ = offset_multifeatures(input, self.num_classes)
+        embed_ = self.embedding(input_) # shape: (*, num_features, num_factor)
+        if self.flatten:
+            return embed_.flatten(start_dim=-2) # shape: (*, num_features*num_factor)
         else:
             return embed_
+        nn.Embedding
