3a2db467364f6cd9c11fdcbddb2f319f22042d87
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-03-09 17:00:23 +0800

    bert model add

--- Code/projs/bert/Dataset.py
+++ Code/projs/bert/Dataset.py
@@ -1,12 +1,11 @@
 import torch
 
-class projDataset(torch.utils.data.Dataset):
-    def __init__(self):
-        pass
-    
-    def __getitem__(self, index):
-        pass
-    
-    def __len__(self):
-        pass
-    
+def get_tokens_segments(tokens_a, tokens_b=None):
+    # input token_a: list of tokens, [tk1, tk2,...,tkN]
+    # input token_b if not none: list of tokens, [tk1, tk2,...,tkN]
+    tokens = ['<cls>'] + tokens_a + ['<sep>']
+    segments = [0] * len(tokens)
+    if tokens_b is not None:
+        tokens += tokens_b + ['<sep>']
+        segments += [1]*(len(tokens_b) + 1)
+    return tokens, segments
--- test.py
+++ test.py
@@ -3,10 +3,17 @@ import warnings
 import torch
 import torch.nn as nn
 warnings.filterwarnings("ignore")
-from Code.projs.word2vec._jobs import skipgram_train_job, cbow_train_job, skipgram_infer_job, cbow_infer_job
+from Code.projs.bert.Network import BERTencoder
 
 if __name__ == "__main__":
     '''
     test code in this test.py file. After successful tests, code will be moved to _jobs.py under proj_name
     '''
-    skipgram_infer_job()
+    vocab_size, num_blks, num_heads, num_hiddens, dropout, ffn_num_hiddens = 10000, 2, 12, 768, 0.2, 1024
+    batch_size, seq_len = 2, 8
+    test_tokens = torch.randint(0, vocab_size, (batch_size, seq_len))
+    test_segments = torch.tensor([[0,0,0,1,1,1,1,1], [0,0,0,0,1,1,1,1]])
+    valid_lens = torch.tensor([8, 8])
+    net = BERTencoder(vocab_size, num_blks, num_heads, num_hiddens, dropout, ffn_num_hiddens, seq_len)
+    out = net(test_tokens, test_segments, valid_lens)
+    print(out.shape)
