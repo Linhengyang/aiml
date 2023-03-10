d42cbd4eca269de7182b0664204cbb1379a2728e
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-03-10 17:09:20 +0800

    bert train job test passed

--- test.py
+++ test.py
@@ -3,15 +3,9 @@ import warnings
 import torch
 import torch.nn as nn
 warnings.filterwarnings("ignore")
-from Code.projs.bert.Dataset import wikitextDataset
+from Code.projs.bert._jobs import train_job
 if __name__ == "__main__":
     '''
     test code in this test.py file. After successful tests, code will be moved to _jobs.py under proj_name
     '''
-    fpath = os.path.join('../../data', 'bert/wikitext-2', 'wiki.train.tokens')
-    max_len = 64
-    testDS = wikitextDataset(fpath, max_len)
-    data_iter = torch.utils.data.DataLoader(testDS, 3, False)
-    for everything in data_iter:
-        print(everything)
-        break
+    train_job()
