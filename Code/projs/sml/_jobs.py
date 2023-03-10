c790058cea97d22463ee83b37dc4cc42a7fcba98
linhengyang.lhy <linhengyang.lhy@alibaba-inc.com>
2023-03-10 21:01:46 +0800

    statistical machine learning proj add

---
+++ Code/projs/sml/_jobs.py
@@ -0,0 +1,15 @@
+import os
+import warnings
+warnings.filterwarnings("ignore")
+import torch
+import torch.nn as nn
+import yaml
+configs = yaml.load(open('Code/projs/sml/configs.yaml', 'rb'), Loader=yaml.FullLoader)
+local_model_save_dir = configs['local_model_save_dir']
+base_data_dir = configs['base_data_dir']
+
+def train_job():
+    pass
+
+def infer_job():
+    pass
