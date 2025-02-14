c4c249699f41cc005c7e53415c3e2d88cfd9273a
Linhengyang <linhengyang1@outlook.com>
2025-02-14 18:34:51 +0800

    test update

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
-import os
 import warnings
-import torch
-import torch.nn as nn
 warnings.filterwarnings("ignore")
-# from Code.projs.bert._jobs import pretrain_job, embed_job
+from Code.Utils.Text.BytePairEncoding import get_BPE_symbols
+import re
 if __name__ == "__main__":
     '''
     test code in this test.py file. After successful tests, code will be moved to _jobs.py under proj_name
     '''
-    # embed_job()
-    print("hello world test")
+    # tail_token: str = '</z>'
+    # # text = "low low lower; lower.   \n  lower high higher\thigh high high"
+    # text = 'lowest highest estimate'
+    # symbols = get_BPE_symbols(text, tail_token, merge_times=2)
+
+    # print(symbols)
+    pattern = re.compile('(.</w>)|(.)')
+    text = 'abcw</w> wa'
+    for match in re.finditer(pattern, text):
+        print(match.group(1))
+        print(match.group(2))
+        print("---")
