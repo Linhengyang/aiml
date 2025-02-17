0c3d1601c581b3d8ff17b10532ca8df56f222457
Linhengyang <linhengyang1@outlook.com>
2025-02-17 20:14:07 +0800

    bpe learner tested

--- test.py
+++ test.py
@@ -6,15 +6,8 @@ if __name__ == "__main__":
     '''
     test code in this test.py file. After successful tests, code will be moved to _jobs.py under proj_name
     '''
-    # tail_token: str = '</z>'
-    # # text = "low low lower; lower.   \n  lower high higher\thigh high high"
-    # text = 'lowest highest estimate'
-    # symbols = get_BPE_symbols(text, tail_token, merge_times=2)
+    tail_token: str = '</z>'
+    text = "?low   low low lower;. lower.   \n  lower high higher\thigh high high big)bigger bigger,bigger.."
+    symbols = get_BPE_symbols(text, tail_token, merge_times=10)
 
-    # print(symbols)
-    pattern = re.compile('(.</w>)|(.)')
-    text = 'abcw</w> wa'
-    for match in re.finditer(pattern, text):
-        print(match.group(1))
-        print(match.group(2))
-        print("---")
+    print(symbols)
