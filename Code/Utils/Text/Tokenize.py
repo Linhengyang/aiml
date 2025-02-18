ef40c1914337125e47ad1eed34b4e28ce9992954
Linhengyang <linhengyang1@outlook.com>
2025-02-18 23:38:46 +0800

    tokenizer add

--- Code/Utils/Text/Tokenize.py
+++ Code/Utils/Text/Tokenize.py
@@ -1,4 +1,14 @@
+# Tokenize.py
+# 本文件收录了各种 tokenize 工具。
+# 至少要输入一条语句 sentence(string)，和一个词汇集 symbols(set|list)。至少输出一个列表 list of tokens(list of string)
+
+
 import typing as t
+import re
+
+
+
+
 
 
 def line_tokenize_simple(
@@ -7,3 +17,7 @@ def line_tokenize_simple(
         ) -> t.List[str]:
     
     return sentence.split(' ')
+
+
+
+def line_tokenize_
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     test code in this test.py file. After successful tests, code will be moved to _jobs.py under proj_name
     '''
     tail_token: str = '</z>'
-    text = "?low   low low lower;. lower.   \n  lower high higher\thigh high high big)bigger bigger,bigger.."
+    text = "fast fast fast fast faster faster faster tall tall tall tall tall taller taller taller taller"
     symbols = get_BPE_symbols(text, tail_token, merge_times=10)
 
     print(symbols)
