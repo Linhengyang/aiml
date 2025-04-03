6cf46120d7d54699920fd99306f07905af3f3a9a
Linhengyang <linhengyang1@outlook.com>
2025-04-03 17:42:50 +0800

    update

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
 # test.py
+from Code.Utils.Text.BytePairEncoding import segment_word_BPE_greedy, get_BPE_symbols
+
+
+if __name__ == "__main__":
+    corpus = "asdfd asdfads dfiuo dfaishd asdjkhd siduafd"
+    symbols = get_BPE_symbols(
+        corpus,
+        tail_token="_",
+        merge_times=10
+    )
+    print(symbols)
+
+    res = segment_word_BPE_greedy(
+        word = "asdffd",
+        symbols = symbols,
+        EOW_token = "_"
+    )
+    print(res)
