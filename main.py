6db56ab5880b1d744c678dce040011ce14316768
Linhengyang <linhengyang1@outlook.com>
2025-04-10 17:27:18 +0800

    main.py update

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 import warnings
 warnings.filterwarnings("ignore")
-from Code.projs.transformer._jobs import train_job
+from Code.projs.transformer._jobs import prepare_job, train_job, infer_job
 
 if __name__ == "__main__":
-    train_job()
+    eng_symbols_path, fra_symbols_path = prepare_job()
+
+    saved_params_fpath = train_job(eng_symbols_path, fra_symbols_path)
+
+    infer_job(saved_params_fpath, eng_symbols_path)
