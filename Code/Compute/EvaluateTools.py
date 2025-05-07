75bd558c2e49aba57d6ae9849fd8adf26315bc5c
linhengyang <linhengyang1@outlook.com>
2025-05-07 19:42:16 +0800

    update

--- Code/Compute/EvaluateTools.py
+++ Code/Compute/EvaluateTools.py
@@ -146,6 +146,7 @@ def accuracy(y_hat: Tensor, y: Tensor):
     if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
         y_hat = y_hat.argmax(dim=1)
     cmp = y_hat.type(y.dtype) == y
+    
     return float(cmp.type(y.dtype).sum())
 
 
