2790a5acefb74aba80786e89725d7bada0975871
linhengyang <linhengyang1@outlook.com>
2025-05-07 10:19:46 +0800

    remove unused object

--- Code/projs/vit/Dataset.py
+++ Code/projs/vit/Dataset.py
@@ -1,11 +1,10 @@
-from ...Utils.Text.TextPreprocess import preprocess_space
-from ...Utils.Text.Vocabulize import Vocab
-from ...Utils.Common.SeqOperation import truncate_pad
 import torch
 import torchvision
 from torchvision import transforms
 import numpy as np
 import struct
+
+
 
 class FMNISTDatasetOnline(torch.utils.data.Dataset):
     def __init__(self, path, is_train, resize):
