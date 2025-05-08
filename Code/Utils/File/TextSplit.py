5fd9fcf7025c49c1fd4412b4c3ec8039a4fae3d0
linhengyang <linhengyang1@outlook.com>
2025-05-08 12:28:13 +0800

    add TextSplit.py

--- Code/Utils/File/TextSplit.py
+++ Code/Utils/File/TextSplit.py
@@ -1,3 +1,34 @@
 # TextSplit.py
+import random
+import typing as t
+import os
+import numpy as np
 
+def split_textfile(input_file_path, ratios:t.List[float], output_dir, shuffle:bool=True):
+    assert all([r > 0 for r in ratios]) and sum(ratios) <= 1.0, \
+        f'ratios shall be positive floats which sum to 1'
+
+    os.makedirs(output_dir, exist_ok=True)
+    _, file_ext = os.path.splitext(input_file_path)
+
+    # 读取所有行
+    with open(input_file_path, 'r') as f:
+        lines = f.readlines()
+    
+    if shuffle:
+        random.shuffle(lines)
+
+    total_line_num = len(lines)
+    split_ends = [0] + ( total_line_num * np.array(ratios).cumsum() ).astype(int).tolist()
+
+    for i in range(1, len(split_ends)):
+        r = ratios[i-1] # 当前文件 所占比例
+
+        cur_part_path = os.path.join(output_dir, str(r).replace('0.', '')+file_ext) # 当前文件名
+
+        with open(cur_part_path, 'w') as f:
+            f.writelines( lines[split_ends[i-1]:split_ends[i]] ) # 把 split_ends[i-1]:split_ends[i] 这些行写入 当前文件
+            print(f'{cur_part_path} created')
+
+    return output_dir
 
