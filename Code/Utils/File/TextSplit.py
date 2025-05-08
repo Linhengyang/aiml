8eff419ac4323f39be799ce983770eff4edf78b0
linhengyang <linhengyang1@outlook.com>
2025-05-08 12:51:56 +0800

    update TextSplit

--- Code/Utils/File/TextSplit.py
+++ Code/Utils/File/TextSplit.py
@@ -22,9 +22,9 @@ def split_textfile(input_file_path, ratios:t.List[float], output_dir, shuffle:bo
     split_ends = [0] + ( total_line_num * np.array(ratios).cumsum() ).astype(int).tolist()
 
     for i in range(1, len(split_ends)):
-        r = ratios[i-1] # 当前文件 所占比例
+        r = ratios[i-1]*100 # 当前文件 所占比例(百分号下)
 
-        cur_part_path = os.path.join(output_dir, str(r).replace('0.', '')+file_ext) # 当前文件名
+        cur_part_path = os.path.join(output_dir, str(r).rstrip('0').rstrip('.')+file_ext) # 当前文件名
 
         with open(cur_part_path, 'w') as f:
             f.writelines( lines[split_ends[i-1]:split_ends[i]] ) # 把 split_ends[i-1]:split_ends[i] 这些行写入 当前文件
