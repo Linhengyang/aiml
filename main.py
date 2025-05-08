b448a84da0b792e6053757f628fe22f3c6bab8f0
linhengyang <linhengyang1@outlook.com>
2025-05-08 23:23:58 +0800

    testing vit

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import os
 import warnings
 warnings.filterwarnings("ignore")
-from Code.projs.transformer._jobs import prepare_job, train_job, infer_job
 
 if __name__ == "__main__":
+
+    # # transformer
+
+    # from Code.projs.transformer._jobs import prepare_job, train_job, infer_job
+    # eng_symbols_path, fra_symbols_path = prepare_job()
+    # # eng_symbols_path, fra_symbols_path = "../cache/text_translator/symbols/source.json", "../cache/text_translator/symbols/target.json"
+
+    # # saved_params_fpath = train_job(eng_symbols_path, fra_symbols_path)
+    # saved_params_fpath = "../model/text_translator/saved_params_2025-05-08_17:32.pth"
+
+    # infer_job(saved_params_fpath, eng_symbols_path)
+
+    # # transformer tested
+
+    # vit
+
+    from Code.projs.vit._jobs import train_job, infer_job
+
+    saved_params_fpath = train_job("local")
     
-    eng_symbols_path, fra_symbols_path = prepare_job()
-    # eng_symbols_path, fra_symbols_path = "../cache/text_translator/symbols/source.json", "../cache/text_translator/symbols/target.json"
+    infer_job(saved_params_fpath)
+    
 
-    # saved_params_fpath = train_job(eng_symbols_path, fra_symbols_path)
-    saved_params_fpath = "../model/text_translator/saved_params_2025-05-08_17:32.pth"
-
-    infer_job(saved_params_fpath, eng_symbols_path)
+    # transformer tested
