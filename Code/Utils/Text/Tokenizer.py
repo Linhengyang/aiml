60373280641af947d9711a4a8e8a8ab9d2d0f49c
linhengyang <linhengyang1@outlook.com>
2025-06-17 17:39:40 +0800

    update

--- Code/Utils/Text/Tokenizer.py
+++ Code/Utils/Text/Tokenizer.py
@@ -6,8 +6,20 @@
 # 2. 可序列化
 # 3. 可扩展
 # 4. 高效
-# 5. 
+# 5. 行为对齐：word with its preceding space are part of the same token，即 单空格作为原始字符串参与tokenize
+#             在 Glossary 中实现的经典 BPE 方法，实际蕴含了一个 “ 预分词 ” 步骤，即整个文本流（添加EOW后），用单空格切分，然后基于切分的结果作 BPE 进一步切分
+#             这里的 用单空格切分，就是 预分词。预分词其实否定了“LLM学习单空格”：即单空格不是需要学习的token（甚至不是需要学习的token的一部分）。
+#             它依赖于语言的空格约定。
 
+#             一, 预分词可以改进，比如英文文献可以使用 GPT2_TOKENIZER_REGEX 来 预分词。
+#             二，预分词可以彻底摒弃：现代的 Tokenizer 比如 SentencePiece，不依赖 pre-tokenize 或 语言的空格约定，它将整个文本流视作一个序列，并学习子词
+#             它将空格显示地包含在 token 中（通常作为前缀），明确地标记了单词的开始，从而简化了逆向转换过程，也能确保信息不丢失。特别适合多语言环境。
+#             在学习过程中，首先把单空格转化为一个特殊、可见的符号（U+2581），然后它与其他字符一起参与BPE学习。由于自然语言中的空格真实频率，大量自带前导空格的
+#             token 会出现在词表中，代表了新word的开始。 好处是1：文本序列（不需要eow） <--> token 序列相互 转换无损，2: 不依赖语言的空格约定，对东亚语言友好
+# 6. 训练过程: 字节级 byte-level BPE 以解决 out-of-vocabulary 问题。BBPE 把所有 原始字符串 转换成 字节序列，然后从 UTF-8 字节序列（而不是 unicoode字符序列）
+#             开始。意味着 初始 词汇表是 256 个 可能的字节，然后 迭代地合并出现最频繁的字节对
+# 7. 推理过程：这个还是 “最长贪婪匹配”。但实现过程不一样：StringSegment.py 里写的是从末尾开始查找尽量长的token，而现代 Tokenizer 一般实现一个 树结构，以便
+#             从头开始高效查找词汇表中的最长匹配（无需遍历整个词汇表）
 
 # meta-class
 from abc import ABC
@@ -24,3 +36,41 @@ class Tokenizer(ABC):
         raise NotImplementedError
     
 
+
+
+class CharacterTokenizer(Tokenizer):
+
+    def encoder(self, string:str) -> t.List[int]:
+        return list( map(ord, string) )
+    
+    def decode(self, indices: t.List[int]) -> str:
+        # filter valid unicode index
+        indices = [ i  for i in indices if 0<= i <= 0x10FFFF ]
+        return "".join(map(chr, indices))
+    
+
+
+class ByteTokenizer(Tokenizer):
+
+    def encoder(self, string:str) -> t.List[int]:
+        string_bytes = string.encode("utf-8") # 返回 utf-8 规范编码的 字节byte 序列. 所以返回的 string_bytes 是一个序列
+        # 可以求 len, 返回 字节数量; 可以索引 index，返回各个 字节的整数值(0-255)
+        # 英文 1 字节，欧洲字符 2字节，中日韩字符 3字节，罕见字符 4字节
+        indices = list( map(int, string_bytes) ) # list of integers btw 0-255
+        return indices
+    
+    def decode(self, indices):
+        # filter valid unicode index
+        try:
+            string_bytes = bytes(indices)
+            return string_bytes.decode('utf-8')
+        except UnicodeDecodeError:
+            print(f'decode error for {indices}')
+
+
+
+
+# 一个 经典的 字符级 BPE 生产的 Tokenizer, 
+class tradTokenizer(Tokenizer):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
