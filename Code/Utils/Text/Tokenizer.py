74e07ebb18c80249d23a582b29a1459f7a2405cd
lhy-Macbook01 <linhengyang1@outlook.com>
2025-06-27 22:01:22 +0800

    raise error rather than early-stop when corpus is ran-out

--- Code/Utils/Text/Tokenizer.py
+++ Code/Utils/Text/Tokenizer.py
@@ -199,8 +199,8 @@ class BBPETokenizer(Tokenizer):
                 f'input explicit_n_vocab (shall be at least greater than 256+{len(special_marks)}(num_special_marks)={256+len(special_marks)}\n' + \
                 f'e.g, GPT2 tokenizer has explicit_n_vocab as 50257, with 1 special marks and 50000 merges'
             
-            # 需要执行 merge 的次数。但不一定能执行到，因为可能存在 run out of corpus 的情况（即所有 corpus 被merge到一起了）
-            self._num_merges = explicit_n_vocab - 256 - len(self._special_marks)
+            # 需要执行 merge 的次数。但不一定能执行到，因为可能存在 run out of corpus 的情况（即所有 corpus 被merge到一起了）。此时 raise error
+            self._num_merges = explicit_n_vocab - 256 - len(special_marks)
 
 
     def _build_vocab(self):
@@ -251,8 +251,14 @@ class BBPETokenizer(Tokenizer):
                 get_pair_counts(tokens, p_counts) # 从当前 tokens 提取 pairs，更新 p_counts
 
             # 如果 经过 chunks_tokens 累积更新 p_counts, p_counts 仍然是 {}, 说明 corpus 已经全部 merge 到一起, 无可 merge
+            # 不提前结束 merge 循环, 而是报错, 提示 更换参数 explicit_n_vocab 或 语料 corpus。原因是参数 explicit_n_vocab 语意 明确的 vocab_size，
+            # 所以不应引入不确定性：当 explicit_n_vocab 和 corpus 冲突时，raise error而不是根据 corpus 确定 explicit_n_vocab
             if not p_counts:
-                break
+                raise RuntimeError(f'run out of corpus(all merged together) after {i} merges.\n'
+                                   f'the maximum valid `explicit_n_vocab` for this corpus is {256+i+len(self._special_marks)}.\n'
+                                   f're-initialize the tokenizer with lower `explicit_n_vocab` in between {256+len(self._special_marks)}'
+                                   f'(zero-merge) & {256+i+len(self._special_marks)}(exactly-ran-out-of current corpus), '
+                                   f'or with enlarged corpus.')
 
             # 从 p_counts 找到 occur-most pair of tokens（two IDs） as top_pair
             occur_most_pair: tuple[int, int] = max(p_counts, key=p_counts.get) 
@@ -265,17 +271,18 @@ class BBPETokenizer(Tokenizer):
             chunks_tokens = [merge_pair(tokens, occur_most_pair, new_token) for tokens in chunks_tokens]
 
             if verbose:
-                print(f'merge {i+1}/{self._num_merges}: {occur_most_pair} -> {new_token}\
-                       [{vocab[new_token]}] had {p_counts[occur_most_pair]} occurences')
+                print(f'merge {i+1}/{self._num_merges}: {occur_most_pair} -> {new_token}'
+                      f'[{vocab[new_token]}] had {p_counts[occur_most_pair]} occurences')
                 
         self._merge_ranks = merge_ranks
         self._vocab = vocab
 
         # explicit_n_vocab = num_actual_merges + 256 + num_specials
-        # 循环正常结束时, num_actual_merges = _num_merges, i = _num_merges - 1 ---> n_vocab = i + 1 + 256 + num_specials
-        # 循环提前结束时, num_actual_merges = i                                ---> n_vocab = i + 256 + num_specials
-        # 不过不管如何，都等于最近的 new_token + 1 + num_specials
-        self.explicit_n_vocab = new_token + 1 + len(self._special_marks)
+        # 循环正常结束时, i = _num_merges - 1,  num_actual_merges = _num_merges ---> n_vocab = i + 1 + 256 + num_specials
+        # 循环提前结束时, i = num_actual_merges                                 ---> n_vocab = i + 256 + num_specials
+        # 都等于最近的 new_token + 1 + num_specials, 除非极端情况下 _num_merges = 0
+        # 所以还是用最稳妥的计算方式
+        self.explicit_n_vocab = 256 + self._num_merges + len(self._special_marks)
 
         # 得到 merge_ranks forrest 之后，注册 special_tokens
         self._register_special_tokens()
@@ -499,6 +506,7 @@ class BBPETokenizer(Tokenizer):
                     L, R = reverse_merge[idx]
                     s_L, s_R = render_bytes(self._vocab[L]), render_bytes(self._vocab[R])
                     f.write(f"[{s_L}] {L} , [{s_R}] {R} -> [{s}] {idx}\n")
+
 
     @property
     def vocab_size(self) -> int:
