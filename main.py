d175e08c028b4acc008e81cdd72b88cfb908d8a1
linhengyang <linhengyang1@outlook.com>
2025-06-30 23:28:42 +0800

    transformer to test

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 import warnings
 warnings.filterwarnings("ignore")
-from src.projs.vit._jobs import prepare_job, train_job, infer_job
+from src.projs.transformer._jobs import prepare_job, train_job, infer_job
 
 if __name__ == "__main__":
-    prepare_job()
-    saved_params_fpath = train_job('local')
-    infer_job(saved_params_fpath)
+    eng_vocab_path, fra_vocab_path = prepare_job()
+    saved_params_fpath = train_job(eng_vocab_path, fra_vocab_path)
+    infer_job(saved_params_fpath, eng_vocab_path, fra_vocab_path)
