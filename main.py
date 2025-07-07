649d5444b683754293626f03ec143fc8af9ba497
linhengyang <linhengyang1@outlook.com>
2025-07-07 16:04:03 +0800

    update bpe_build()

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
 import warnings
 warnings.filterwarnings("ignore")
-from src.projs.transformer._jobs import prepare_job, train_job, infer_job
+from src.apps.bpe_build._jobs import bpe_build
 
 if __name__ == "__main__":
-    eng_vocab_path, fra_vocab_path = prepare_job()
-    saved_params_fpath = train_job(eng_vocab_path, fra_vocab_path)
-    infer_job(saved_params_fpath, eng_vocab_path, fra_vocab_path)
+    tok_fpath, vocab_cache_dir = bpe_build()
--- src/apps/bpe_build/_jobs.py
+++ src/apps/bpe_build/_jobs.py
@@ -4,11 +4,47 @@ import pyarrow.parquet as pq
 import pyarrow as pa
 import os
 import regex as re
-
-
-
-
+from ...core.utils.text.tokenizer import baseBBPETokenizer, bufferBBPETokenizer
 
 configs = yaml.load(open('src/apps/bpe_build/configs.yaml', 'rb'), Loader=yaml.FullLoader)
-train_pq = configs['train_pq']
-valid_pq = configs['valid_pq']
+
+################## directories ##################
+
+
+################## buffer/save in workspace/cache ##################
+buffer_dir = os.path.join( configs['cache_dir'], configs['app_name'], 'buffer' )
+tokenizer_save_dir = os.path.join( configs['artifact_dir'], configs['app_name'], 'tokenizer' )
+vocab_cache_dir = os.path.join( configs['artifact_dir'], configs['app_name'], 'vocab' )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def bpe_build():
+    print('begin to run BPE on dataset TinyStories with 50k vocab_size')
+    train_pq = configs['train_pq']
+    valid_pq = configs['valid_pq']
+
+    tok = bufferBBPETokenizer(name='tinyTok_50k', buffer_dir=buffer_dir)
+    tok.train_bpe(train_pq, tetext_column='text', num_merges=50000)
+
+    # save tokenizer
+    tok_fpath = os.path.join(tokenizer_save_dir, f'{tok.name}.tok')
+    tok.save(tok_fpath)
+    # view vocab
+    tok.view(tmpsave_dir = vocab_cache_dir)
+    
+    print('BPE ends')
+    return tok_fpath, vocab_cache_dir
