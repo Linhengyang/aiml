bd27c62f8581d1045fa5e883888dbc75bc7ad497
linhengyang <linhengyang1@outlook.com>
2025-07-16 17:55:22 +0800

    update

--- src/lib/tokenizer/setup.py
+++ src/lib/tokenizer/setup.py
@@ -14,7 +14,13 @@ print("Include dirs:", include_dirs)
 ext_modules = [
     Extension(
         name="merge_pair",  # 输出模块名 (import 时用的名字)
-        sources=["merge_pair.pyx", "merge_pair_api.cpp", "merge_pair_core.cpp"],  # 包含 .pyx 和 C++ 源文件
+        sources=[
+            "merge_pair.pyx",
+            "merge_pair_api.cpp",
+            "merge_pair_core.cpp",
+            os.path.join(share_dir,"memory_block.cpp"),
+            os.path.join(share_dir,"memory_pool.cpp"),
+            ],  # 包含 .pyx 和所有涉及到的 C++ 源文件
         language="c++",
         include_dirs=include_dirs,  # 包含 numpy 的头文件路径
         extra_compile_args=["-O3", "-std=c++17"],       # 优化编译选项
