deafb26567a34d5104370db73c14f65866f34cf1
linhengyang <linhengyang1@outlook.com>
2025-07-18 14:23:25 +0800

    update

--- src/apps/bpe_build/_jobs.py
+++ src/apps/bpe_build/_jobs.py
@@ -17,6 +17,9 @@ tokenizer_save_dir = os.path.join( configs['artifact_dir'], configs['app_name'],
 vocab_cache_dir = os.path.join( configs['artifact_dir'], configs['app_name'], 'vocab' )
 
 
+################## backup init to data/TinyStories/bytes ##################
+backup_init_dir = configs['backup_init_dir']
+
 
 
 
@@ -38,18 +41,22 @@ def bpe_prepare():
 
 
 
-def bpe_build():
+def bpe_train():
     print('begin to run BPE on dataset TinyStories')
     train_pq = configs['train_pq']
     valid_pq = configs['valid_pq']
     for folder in [buffer_dir, tokenizer_save_dir, vocab_cache_dir]:
         os.makedirs(folder, exist_ok=True)
         
-    tok = boostBBPETokenizer(name='tinyTok_3', buffer_dir=buffer_dir)
+    tok = boostBBPETokenizer(name='tinyTok', buffer_dir=buffer_dir)
     corpora = [valid_pq,]
-    text_columns=['text',]
+    colnames=['text',]
 
-    tok.train_bpe(corpora, text_columns, num_merges=3, verbose=True)
+    tok.train_bpe(0,
+                  corpora=corpora,
+                  colnames=colnames,
+                  backup_init_tokens_dir=backup_init_dir
+                  )
 
     # save tokenizer
     tok_fpath = os.path.join(tokenizer_save_dir, f'{tok.name}.tok')
@@ -58,26 +65,32 @@ def bpe_build():
     tok.view(tmpsave_dir = vocab_cache_dir)
     
     print('BPE ends')
-    return tok_fpath, vocab_cache_dir
 
 
 
 
-def bpe_continue(continue_num_merges):
+
+def bpe_continue():
     print('continue to run BPE on dataset TinyStories')
-    tok = boostBBPETokenizer(name='init', buffer_dir=buffer_dir)
-    # load tokenizer
-    tok.load(os.path.join(tokenizer_save_dir, 'tinyTok_6.tok'))
 
-    # continue to train `continue_num_merges` epoch
-    tok.continue_bpe(continue_num_merges)
+    train_pq = configs['train_pq']
+    valid_pq = configs['valid_pq']
+    for folder in [buffer_dir, tokenizer_save_dir, vocab_cache_dir]:
+        os.makedirs(folder, exist_ok=True)
+    
+    corpora = [valid_pq,]
+    colnames=['text',]
 
-    # save/view the new tokenizer in different name
-    tok.name = 'tinyTok_13'
-    tok_fpath = os.path.join(tokenizer_save_dir, f'{tok.name}.tok')
+    tok = boostBBPETokenizer(name='tinyTok', buffer_dir=buffer_dir)
+    tok_path = os.path.join(tokenizer_save_dir, f'tinyTok.tok')
+    tok.load(tok_path)
+
+    tok.train_bpe(4,
+                  corpora=corpora,
+                  colnames=colnames,
+                  backup_init_tokens_dir=backup_init_dir)
+    # save tokenizer
+    tok_fpath = os.path.join(tokenizer_save_dir, f'{tok.name}_new.tok')
     tok.save(tok_fpath)
-    # view vocab
-    tok.view(tmpsave_dir = vocab_cache_dir)
-
-    print(f'{continue_num_merges} BPE ends')
-    return tok_fpath, vocab_cache_dir
+    
+    print('BPE ends')
