ab4aefe4e462123736732bb6f9bd943acf227bba
linhengyang <linhengyang1@outlook.com>
2025-08-06 19:56:52 +0800

    catch bug

--- src/lib/tokenizer/global_resource.cpp
+++ src/lib/tokenizer/global_resource.cpp
@@ -1,17 +1,18 @@
 // global_resource.cpp
 #include "global_resource.h"
 #include "memory_pool_global.h"
+#include <cstddef>
+#include "mempool_counter.h"
 
 // 初始化为 nullptr
 counter<counter_key_type, false>* global_counter_st = nullptr;
 counter<counter_key_type, true>* global_counter_mt = nullptr;
 
-
 void init_global_counter(size_t capacity) {
-    global_mempool* pool = &global_mempool::get(4096, 8);
-
+    mempool_interface* pool = &global_mempool::get();
+    
     if (!global_counter_st) {
-        global_counter_st = new counter<counter_key_type, false>{capacity, pool};
+        global_counter_st = new counter<counter_key_type, false>(capacity, pool);
     }
     if (!global_counter_mt) {
         global_counter_mt = new counter<counter_key_type, true>(capacity, pool);
