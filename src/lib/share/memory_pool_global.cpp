160d01670e1dc8b02520e9c07b146a2c3175f1b4
linhengyang <linhengyang1@outlook.com>
2025-08-06 14:00:10 +0800

    now specify singleton global memory pool as global_mempool class

--- src/lib/share/memory_pool_global.cpp
+++ src/lib/share/memory_pool_global.cpp
@@ -1,6 +1,6 @@
-// memory_pool.cpp
+// global_mempool.cpp
 
-#include "memory_pool.h"
+#include "memory_pool_global.h"
 #include "memory_block.h"
 #include <stdexcept>
 #include <cstdint>
@@ -15,16 +15,14 @@
 
 // 定义静态成员变量
 // 用智能指针来管理单例
-std::unique_ptr<memory_pool, memory_pool::Deleter> memory_pool::_instance = nullptr;
-std::mutex memory_pool::_mtx;
-
-
+std::unique_ptr<global_mempool, global_mempool::Deleter> global_mempool::_instance = nullptr;
+std::mutex global_mempool::_mtx;
 
 
 
 
 // 构造函数. 放到 private 里导致它不对外使用.
-memory_pool::memory_pool(size_t block_size, size_t alignment):
+global_mempool::global_mempool(size_t block_size, size_t alignment):
     _block_size(block_size),
     _blocks(std::vector<block*>()),
     _large_allocs(std::vector<void*>()),
@@ -43,8 +41,8 @@ memory_pool::memory_pool(size_t block_size, size_t alignment):
 }
 
 
-
-memory_pool::~memory_pool() {
+// 析构函数. 放到 private 里导致外部不可使用（所以不加锁）. 必须通过公共接口destroy/release调用. 这些方法会加锁
+global_mempool::~global_mempool() {
     // 析构时调用不加锁的 release_no_lock 方法. 因为析构已经被私有, 而释放内存池的公共接口 mempool_destroy 会带锁
     // 所以为了避免死锁, 这里无需
     release_no_lock();
@@ -52,7 +50,7 @@ memory_pool::~memory_pool() {
 
 
 
-void memory_pool::release_no_lock() {
+void global_mempool::release_no_lock() {
     // 不加锁的 release 内存池, 给加锁的release等外部函数使用, 在外部加锁使用
 
     for(auto block: _blocks) {
@@ -76,19 +74,19 @@ void memory_pool::release_no_lock() {
 
 
 
-void memory_pool::release() {
+void global_mempool::release() {
 
     // public方法 release 涉及到修改共享变量, 加锁只能由一个线程执行.
     std::lock_guard<std::mutex> lock(_mtx);
 
-    memory_pool::release_no_lock();
+    global_mempool::release_no_lock();
 
 }
 
 
 
 // 申请分配 size 字节数量的内存.
-void* memory_pool::allocate(size_t size) {
+void* global_mempool::allocate(size_t size) {
 
     std::lock_guard<std::mutex> lock(_mtx);
 
@@ -143,8 +141,8 @@ void* memory_pool::allocate(size_t size) {
 
 
 //如果 ptr是大对象(大于_block_size)的，会被释放;否则不会被释放
-//如果ptr在 _large_allocs 里，那么释放它，然后从 _large_allocs中删除它。否则不作任何操作
-void memory_pool::dealloc_large(void* ptr) {
+//如果ptr在 _large_allocs 里，那么释放它，然后从 _large_allocs 中删除它。否则不作任何操作
+void global_mempool::dealloc_large(void* ptr) {
     // 单独释放大内存, 加互斥锁
     std::lock_guard<std::mutex> lock(_mtx);
 
@@ -161,8 +159,8 @@ void memory_pool::dealloc_large(void* ptr) {
 
 
 
-// reset 内存池: reset 所有内存block, release 所有 large_allocation
-void memory_pool::reset() {
+// reset 内存池: reset 所有内存 block, release 所有 large_allocation
+void global_mempool::reset() {
 
     std::lock_guard<std::mutex> lock(_mtx);
 
@@ -197,7 +195,7 @@ void memory_pool::reset() {
 // 为了防止大起大落（第一波用了很多内存块，第二波用了很少，第三波又用了很多），每次shrink最多max_num个
 // 必须要在 reset之前用. 因为reset会重置所有block._offset=0
 // 下一步紧接着就是 reset
-void memory_pool::shrink(size_t max_num) {
+void global_mempool::shrink(size_t max_num) {
     std::lock_guard<std::mutex> lock(_mtx);
 
     // 如果当前block指向空, 说明还没有创建任何一个block, 那么无可shrink，直接退出
--- src/lib/share/memory_pool_global.h
+++ src/lib/share/memory_pool_global.h
@@ -1,4 +1,4 @@
-// memory_pool.h
+// global_mempool.h
 
 #ifndef MEMORY_POOL_H
 #define MEMORY_POOL_H
@@ -16,13 +16,13 @@
 // void* ptr;
 // int* pInt = statc_cast<int*>(ptr);
 
-class memory_pool {
+class global_mempool {
 
 private:
 
     // 构造和析构函数写在private里，保证只能由 get_mempool 方法创建内存池实例
-    explicit memory_pool(size_t block_size = 4096, size_t alignment = 8); //默认给单个内存block申请 4kb 的内存, 8字节对齐
-    // explicit的意思是禁止对 memory_pool 类对象作隐式转换
+    explicit global_mempool(size_t block_size = 4096, size_t alignment = 8); //默认给单个内存block申请 4kb 的内存, 8字节对齐
+    // explicit的意思是禁止对 global_mempool 类对象作隐式转换
 
     // 析构函数被private, 导致如下后果:
     // 1. 禁止在栈上创建对象，对象离开作用域时，编译器需要调用析构函数，而它不能访问private
@@ -31,7 +31,7 @@ private:
     // 4. 无法作为函数的返回值(按值返回)，因为返回过程中临时对象需要被析构
     // 5. 禁止std::vector/std::list等标准容器存储, 因为容器需要访问元素的析构函数
     // 单例模式下的对象，适合把析构函数private：这样可以由 唯一指定的接口来释放销毁对象，以此手动控制单例的生命周期
-    ~memory_pool();
+    ~global_mempool();
 
     const size_t _block_size; //内存池中, 单个内存block的字节量
 
@@ -49,8 +49,8 @@ private:
 
     // 自定义删除器结构体
     struct Deleter {
-        void operator()(memory_pool* ptr) const {
-            delete ptr;  // 这里可以访问 private 的析构. 删除器内部不加锁, 因为调用删除器前 mempool_destroy 会加锁
+        void operator()(global_mempool* ptr) const {
+            delete ptr;  // 这里可以访问 private 的析构. 删除器内部不加锁, 因为调用删除器前 destroy 会加锁
         }
     };
 
@@ -58,53 +58,56 @@ private:
     friend struct Deleter;
 
     // 声明静态成员变量 单例指针
-    static std::unique_ptr<memory_pool, Deleter> _instance;
+    static std::unique_ptr<global_mempool, Deleter> _instance;
 
     // 声明静态成员变量 互斥锁
     static std::mutex _mtx;
     // 互斥锁, 保证多线程安全。比如在allocate方法里加入互斥锁，那么会发生如下：
-    // 线程A在函数域内调用对象mempool的allocate方法以获得内存，这个时候lock_guard类就在线程A的作用域里生成了，传入互斥量mtx_，生成对象 lock 
-    // 这个时候如果另一个线程试图调用mempool的allocate方法，它就拿不到互斥量mtx_，所以就只能堵塞，这样就能保证线程A allocate拿到的内存只有线程A能用。
+    // 线程A在函数域内调用对象 global_mempool 的allocate方法以获得内存，这个时候lock_guard类就在线程A的作用域里生成了，
+    // 传入互斥量mtx_，生成对象 lock. 这个时候如果另一个线程试图调用 global_mempool 的allocate方法，它就拿不到互斥量mtx_，
+    // 所以就只能堵塞，这样就能保证线程A allocate拿到的内存只有线程A能用。
     // 当线程A函数在return后，对象 lock 就离开了线程A的作用域，自动销毁，那么互斥量mtx_就被释放了
 
     // 禁止拷贝构造和赋值操作
-    memory_pool(const memory_pool&) = delete;
-    memory_pool& operator=(const memory_pool&) = delete;
+    global_mempool(const global_mempool&) = delete;
+    global_mempool& operator=(const global_mempool&) = delete;
 
 
 public:
     // 类的静态方法不依赖实例（故也不能使用非静态成员/方法），适合用来定义单例：用单例类就可以调用，单例类约等于单例实例
-
-    // 带参数调用: 创建 memory_pool 单例
-    static memory_pool& get_mempool(size_t block_size, size_t alignment) {
+    // 调用方法:  类名::静态方法名
+    // 带参数调用: 创建 global_mempool 单例
+    static global_mempool& get(size_t block_size, size_t alignment) {
         std::lock_guard<std::mutex> lock(_mtx);
         if (!_instance) {
-            _instance.reset(new memory_pool(block_size, alignment));
-            // _instance = std::unique_ptr<memory_pool, Deleter>(new memory_pool(block_size, alignment));
+            _instance.reset(new global_mempool(block_size, alignment));
+            // _instance = std::unique_ptr<global_mempool, Deleter>(new global_mempool(block_size, alignment));
         }
         return *_instance;
     }
 
     // 不带参数调用: 获得已经实现的单例（加锁以保证线程安全）. 若尚未创建，报错
-    static memory_pool& get_mempool() {
+    static global_mempool& get() {
         std::lock_guard<std::mutex> lock(_mtx);
         if(!_instance) {
-            throw std::runtime_error("memory_pool not created");
+            throw std::runtime_error("global_mempool not created");
         }
         return *_instance;
     }
 
     // 探知单例是否已经创建
-    static bool mempool_exist() {
+    static bool exist() {
         std::lock_guard<std::mutex> lock(_mtx);
         return _instance != nullptr;
     }
 
-    // 带锁释放内存池的公共接口，效果和release相同，但是是静态方法，可以并推荐用 类名::接口 来使用
-    static void mempool_destroy() {
+    // 带锁释放内存池的公共接口，效果和release相同，但是是静态方法，可以并推荐用 类名::方法 来使用
+    static void destroy() {
         std::lock_guard<std::mutex> lock(_mtx); // 加锁
         _instance.reset(); // 智能指针 reset 会调用 删除器, 删除器作为友好类, 可以访问访问 private 的析构
     }
+
+    // 非静态方法，调用方法:  实例.方法名, 单例模式下这样调用：类名::get().方法名
 
     // 分配指定大小的内存, 非静态, 依赖成员变量 _blocks 等
     void* allocate(size_t size); // 如果当前块不足以容纳, 则申请新块
@@ -116,7 +119,7 @@ public:
     // 复用内存池的公共接口
     void reset(); // 全部复用，所有已经申请好的内存block都reset成从头可用
 
-    // 带锁释放内存池的公共接口，效果和 destroy 相同，但是是非静态方法，推荐使用 实例.接口 来使用
+    // 带锁释放内存池的公共接口，效果和 destroy 相同，但是是非静态方法
     void release(); // 全部释放(block 和 large alloc都释放), 带锁以线程安全
 
 };
