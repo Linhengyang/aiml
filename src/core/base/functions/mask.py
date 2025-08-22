44de3d880e6ce3a2004caad997988f5459dfd78e
linhengyang <linhengyang1@outlook.com>
2025-08-22 17:08:58 +0800

    update

--- src/core/base/functions/mask.py
+++ src/core/base/functions/mask.py
@@ -10,17 +10,17 @@ def mask_on_last_dim(tensor_shape, mask_lens, mask_flag=True):
     '''
     input:
         tensor_shape: (..., d_size)
-        mask_lens: tensor with shape as (...,) int
+        mask_lens: tensor with shape as (...,), dtype as int
         mask_flag: 指定 mask 部分是 True 还是 False
-        
+    return:
+        mask: (..., d_size)
+        mask 是一个 True/False tensor, 它的 shape 等于 输入参数 tensor_shape.
+    
+    explain:
         mask_lens 中的单个元素用位置(...,)确定后, 在原 tensor 中有唯一的长度为 d_size 的 1-D tensor 与其对应,
         mask_lens 元素值代表了这个 1-D tensor 从index 0(包括)开始, 有多少个是要 mask 的
 
         mask_lens 元素值可以为 0, 代表原 tensor 在该位置的长度为 d_size 的1-D tensor 所有都不需要mask.
-    
-    return:
-        mask: (..., d_size)
-        mask 是一个 True/False tensor, 它的 shape 等于 输入参数 tensor_shape.
     '''
     # index_1dtensor: [0, 1, ..., n_logits-1]
     index_1dtensor = torch.arange(tensor_shape[-1], device=mask_lens.device, dtype=mask_lens.dtype)
