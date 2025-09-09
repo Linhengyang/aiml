e6b15cbf42915d5a7a2bcce5cc0ca56871dfc715
linhengyang <linhengyang1@outlook.com>
2025-09-09 18:37:35 +0800

    test RoPE

--- test.py
+++ test.py
@@ -1,32 +1,23 @@
 # test.py
 import sys
 import typing as t
+import torch
 
 cache_play = '../cache/playground/'
 
 
-from src.core.utils.text.tokenizer import asyncBBPETokenizer
+# from src.core.utils.text.tokenizer import asyncBBPETokenizer
 import pyarrow as pa
+from src.core.nn_components.root_layers.position_encoding import RotaryPosEnc, RoPEConfig
 
 if __name__ == "__main__":
-    token_dtype = pa.uint16()
-    p_counts_schema = pa.schema([
-        pa.field('L', token_dtype),
-        pa.field('R', token_dtype),
-        pa.field('counts', pa.uint64()),
-        ])
-    tokens_schema = pa.schema([
-        pa.field( 'tokens', pa.large_list(pa.field('token', token_dtype)) ),
-        ])
-    
-    def encode_to_ints(s:str, encoding='utf-8') -> t.List[int]:
-        return list( s.encode(encoding) )
-    
-    chunks_tokens = [[1,2,3], [], [3,4,5], [1], [2,6,5,6], [1, 1]]
+    rope_config = RoPEConfig(dim=4)
+    rope = RotaryPosEnc(rope_config)
+    position_ids = torch.tensor([0,1,2,3]).unsqueeze(0)
+    cos1, sin1 = rope.get_sin_cos(position_ids)
 
-    table = pa.Table.from_pydict({tokens_schema[0].name: chunks_tokens}, tokens_schema)
-    batch = table.to_batches()[0]
+    position_id = torch.tensor([2]).unsqueeze(0)
+    cos2, sin2 = rope.get_sin_cos(position_id)
 
-    tokens_flat = batch[tokens_schema[0].name].values.to_numpy()
-
-    offsets = batch[tokens_schema[0].name].offsets.to_numpy()
+    print("cos1: ", cos1)
+    print("cos2: ", cos2)
