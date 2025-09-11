78d32db8233e11ab18d981fc1f20d2cbea7be3f6
linhengyang <linhengyang1@outlook.com>
2025-09-11 14:55:46 +0800

    update

--- src/core/nn_components/sub_modules/_gpt2.py
+++ src/core/nn_components/sub_modules/_gpt2.py
@@ -1,8 +1,6 @@
 import torch
 from torch import nn
-from ..root_layers.layer_normalization import AddLayerNorm
 from ..root_layers.attention_pool import MultiHeadAttention
-from ..root_layers.feedforward import PositionWiseFFN
 
 
 
