c176e3d8add7b79181e10b3a796013e4378fb534
linhengyang <linhengyang1@outlook.com>
2025-09-11 18:16:29 +0800

    update

--- src/projs/gpt2/network.py
+++ src/projs/gpt2/network.py
@@ -1,5 +1,5 @@
-from ...core.nn_components.meta_frames import Encoder, AttentionDecoder, EncoderDecoder
-from ...core.nn_components.root_layers.position_encoding import TrigonoAbsPosEnc
+from ...core.nn_components.meta_frames import Decoder, DecoderOnly
+from ...core.nn_components.root_layers.position_encoding import LearnAbsPosEnc
 from ...core.nn_components.sub_modules._gpt2 import GPT2DecoderBlock
 import torch.nn as nn
 import math
@@ -9,5 +9,5 @@ import torch
 
 
 
-class gpt2(AttentionDecoder):
+class gpt2(DecoderOnly):
     pass
