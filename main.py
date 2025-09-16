80637217c9cd536e4908984f6851422fd69adecd
linhengyang <linhengyang1@outlook.com>
2025-09-16 18:00:12 +0800

    add test gpt2 job

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import os
 import warnings
 warnings.filterwarnings("ignore")
-from src.apps.bpe_build._jobs import bpe_prepare, bpe_train, bpe_continue
-from src.core.utils.common.performance import timeit
+from src.projs.gpt2._jobs import test_job
 
 if __name__ == "__main__":
-    bpe_prepare()
-    with timeit():
-        # tok_path=None表示不load, 从0开始续train
-        bpe_continue(tok_path=None)
+    test_job()
 
 
 
