9803d548aaebfe2c7f29efb2128a042fa299007e
linhengyang <linhengyang1@outlook.com>
2025-09-17 16:44:08 +0800

    update

--- src/projs/gpt2/dataset.py
+++ src/projs/gpt2/dataset.py
@@ -1,3 +1,31 @@
+# dataset for GPT
+# 如何 从 corpus 生产 input_seqs 和 label_seqs?
+# 1. 确定 L_q. L_q 小于等于 MAX_CONTEXT_SIZE, 最好接近它以保证宣称的效果. 不同 batch 的 L_q 可以不同, 有些模型在 epoch 加大时, 逐渐增大 L_q
+# 2. corpus 各 text 末尾都要 append ENDOFTEXT 符, 以保证 ENDOFTEXT TOKEN 以正确的方式进入 label_seqs, 从而让模型正确学习何时该生成 ENDOFTEXT
+# 3. text 是否考虑 packing 成一条序列? 作 ENDOFTEXT 进入 input_seqs 影响分析:
+#    考虑 corpus 'abcd0', 0 是 ENDOFTEXT TOKEN. 产出 input_seqs 'abcd', label_seqs 'bcd0'. 可见 ENDOFTEXT 不进入 input_seq时, 对齐是自然的
+#    考虑 两个text packing: 'abc0def0', 0 是 ENDOFTEXT TOKEN. 产出 input_seqs 'abc0def', label_seqs 'bc0def0'. 我们列出 自回归系数矩阵:
+#          a  b  c  0  d  e  f
+#       a  1                     --> b
+#       b  1  1                  --> c
+#       c  1  1  1               --> 0
+#       0  1  1  1  1            --> d
+#       d  1  1  1  1  1         --> e
+#       e  1  1  1  1  1  1      --> f
+#       f  1  1  1  1  1  1  1   --> 0
+#    从上往下数, 一直到 c --> 0 都是正常的. 但是下一行 0 --> d 就值得商榷了: ENDOFTEXT 作为 q, 前文 abc0 作为 kv, label 是下一篇文章的开头 d?
+#    再往下看, d --> e: d 作为 q, ENDOFTEXT 和前一篇文章的 abc 作为 kv, label是本文的 e?
+#    可以看出, 若 ENDOFTEXT token 进入 input_seqs, 会混淆前后两篇不相干的文章, 会干扰模型的上下文, 干扰模型的学习能力.
+#    所以一般不把 ENDOFTEXT token 做入 input_seqs. ???
+
+
+# 4. corpus 切分成 input_seqs 时, 是否考虑 overlapping? 考虑对于 corpus 'abcdef0', 0代表 ENDOFTEXT, L_q = 3, 产出 data-label pair
+#       方案一 no-overlapping: (abc, bcd), (def, ef0)
+#       方案二 overlap 1, stride = 2: (abc, bcd), (cde, def), (ef<PAD>, f0<PAD>)
+#       方案三 overlap 2, stride = 1: (abc, bcd), (bcd, cde), (cde, def), (def, ef0), (ef<PAD>, f0<PAD>), (f<PAD><PAD>, 0<PAD><PAD>)
+#    首选方案一: 样本相关性最低, 并与 packing+分段因果遮罩 天然相容. 当小样本微调时, 可以考虑方案二: overlap要小/stride要大, 尽量不PAD(packing或丢弃)
+
+
 import torch
 
 class projDataset(torch.utils.data.Dataset):
--- src/projs/gpt2/loss.py
+++ src/projs/gpt2/loss.py
@@ -1,1 +1,43 @@
 # Loss for GPT
+# network of gpt2 forward during train:
+# INPUT:
+#   input_seqs: data of tensor[B, L_q]int64, L_q <= MAX_CONTEXT_SIZE
+#       MAX_CONTEXT_SIZE 是模型的参数, 会被用在 casual_mask / position encoding 两个结构部分(可能). 代表最大的 因果遮罩大小 / 可编程位置数量.
+#       L_q 是 data batch 的 sequence length(PAD后). 在 单一 batch 内保持固定, 不同 batch 可以不同, 但 L_q 始终小于等于 MAX_CONTEXT_SIZE
+#       由于 MAX_CONTEXT_SIZE 是 模型宣称的参数, 意指 该模型能捕捉 最大为该值的上下文关系. 为了达到宣称的效果, 实际训练的 L_q 应该接近 MAX_CONTEXT_SIZE.
+#   attention_mask: None 仅当 input_seqs 全部都不是 PAD. / tensor [B, L_q]bool, 对 input_seqs 的 PAD-info 描述. False-->PAD, True-->non-PAD.
+#       PAD 和 EODOFTEXT 分隔符等特殊作用符不是一个概念. 特殊作用符号是valid token, 即它们正常参与模型计算和参数更新. 
+#       PAD 则不同, 它们会被 attention_mask 从运算中彻底屏蔽掉, 不参与模型运算. PAD 仅仅只是为了补齐 batch 内 部分序列样本 到一致长度. 尽量减少PAD.
+#       因为 PAD 会在运算中被彻底屏蔽, 所以用 ENDOFTEXT等特殊作用符 来当作 PAD 只是为了复用符号, 并不是说 PAD 位置 是 TOKEN.
+#   past_kv: None
+#   past_attention_mask: None
+#   if_cache_kv: False
+
+# OUTPUT:
+# 1. logits: tensor [B, L_q, vocab_size]float
+# 2. None
+# 3. attention_mask: INPUT 的 attention_mask 原样输出
+
+# LABEL:
+# label_seqs: label of tensor[B, L_q]int64
+#       label_seqs 与 input_seqs 的对齐 align:
+#           对于 input_seqs 的 PAD      <----------------=---->     label_seqs 也是 PAD
+#           对于 input_seqs 的 TOKEN    ---原corpus往后移1位--->     label_seqs 的 TOKEN
+# 所以可以看出, attention_mask 同时也是 label_seqs 的 PAD-info. 所以它直接参与 构造 label_mask.
+# label_mask 还需要其他一些信息, 以继续屏蔽一些 sequence position. 比如 text-packing seqs 里, label 里每个 text 的 first-token 不计loss
+
+# 考虑 两个text packing: '<PAD><PAD>abc0def0', 0 是 <PAD>/ENDOFTEXT TOKEN. 产出 input_seqs '00abc0def', label_seqs '00bc0def0'. 自回归系数矩阵:
+#        0  0  a  b  c  0  d  e  f
+#     0  x                              --> 0 (mask because PAD)
+#     0  x  x                           --> 0 (mask because PAD)
+#     a  x  x  1                        --> b
+#     b  x  x  1  1                     --> c
+#     c  x  x  1  1  1                  --> 0
+#     0  x  x  1  1  1  1               --> d (mask because FIRST-TOKEN-OF-PACKING-TEXT)
+#     d  x  x  /  /  /  /  1            --> e
+#     e  x  x  /  /  /  /  1  1         --> f
+#     f  x  x  /  /  /  /  1  1  1      --> 0
+
+# 最终实际 attention_mask = casual_mask ^ nonpad_mask ^ sametext_mask
+# 最终实际 label_maks = notpad ^ sametext_with_prev_token
+# 这里 ^ 是 且运算
