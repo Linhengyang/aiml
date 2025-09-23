07d0dfcd8cdc02bc8b239be517c07efddeb402a7
linhengyang <linhengyang1@outlook.com>
2025-09-23 13:04:01 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import warnings
 warnings.filterwarnings("ignore")
-from src.projs.gpt2._jobs import test_job
+from src.projs.gpt2._jobs import test_job, build_tokenizer_job
 
 if __name__ == "__main__":
-    test_job()
+    build_tokenizer_job()
 
 
 
--- src/projs/gpt2/_jobs.py
+++ src/projs/gpt2/_jobs.py
@@ -14,9 +14,20 @@ configs = yaml.load(open('src/projs/gpt2/configs.yaml', 'rb'), Loader=yaml.FullL
 ################## directories ##################
 # set train log file path / network resolve output path / params save path / source&targe vocabs path
 
-################## symbols and vocabs in workspace/cache ##################
+################## tokenizer in workspace/artifact ##################
 tokenizer_dir = os.path.join( configs['artifact_dir'], configs['proj_name'], 'tokenizer' )
+
+
+
+
+################## view vocab in workspace/tmp ##################
 vocab_dir = os.path.join( configs['tmp_dir'], configs['proj_name'], 'vocab' )
+
+
+
+
+################## buffer directory for BPE in workspace/cache ##################
+buffer_dir = os.path.join( configs['cache_dir'], configs['proj_name'], 'buffer')
 
 
 
@@ -80,13 +91,10 @@ def build_tokenizer_job():
     print('build_tokenizer_job begin')
 
     # create all related directories if not existed
-    for dir_name in [tokenizer_dir, vocab_dir, model_dir, log_dir]:
+    for dir_name in [tokenizer_dir, vocab_dir, model_dir, log_dir, buffer_dir]:
         os.makedirs(dir_name, exist_ok=True)
         print(f'directory {dir_name} created')
-
-    # generate tokenizer
-    # not use BPE
-
+    
     # 读取全部语料 corpus
     print('get full corpus')
     with open(configs['train_data'], 'r', encoding='utf-8') as f:
@@ -100,7 +108,7 @@ def build_tokenizer_job():
     if not os.path.exists(tokenizer_path):
         # create tokenizer
         print('bpe train begin')
-        gpt_tokenizer = boostBBPETokenizer(name='mt')
+        gpt_tokenizer = boostBBPETokenizer(name='mt', buffer_dir=buffer_dir, special_marks=[ENDOFTEXT])
         gpt_tokenizer.train_bpe(30000, corpora=corpus, verbose=True)
         print('bpe train close')
         gpt_tokenizer.save(tokenizer_path)
@@ -118,55 +126,14 @@ def build_tokenizer_job():
 
 
 
-def pretrain_job(vocab_path):
-    pass
+def tokenize_save_job(tokenizer, corpora_paths: t.List[str], save_dir: str):
+    # load tokenizer. name 以及其他参数都会被 load 覆盖
+    tok = boostBBPETokenizer(name='to_load', buffer_dir=buffer_dir)
+    tok.load(tokenizer)
 
-
-
-
-def test_job():
-    
-    test_gpt2_cfg = gpt2Config(
-        embd_size = 64,
-        vocab_size = 27, # a-z <-> 1-26, <eot> <-> 0
-        embd_p_drop = 0.1,
-        ## decoder-block(casual_attention layer + ffn) configs
-        # embd_size:int
-        num_head = 2,
-        use_bias = False,
-        max_context_size = 6,
-        attn_p_drop = 0.1,
-        resid_p_drop = 0.1,
-        use_cached_casual_mask = False,
-        use_rope = True,
-        ## number of decoder-block
-        num_block = 2
-    )
-    chartok = CharacterTokenizer()
-    gpt2_model = gpt2(test_gpt2_cfg)
-
-    corpus = ['abcde', 'heell', 'hello']
-    input_seqs = torch.tensor( [ chartok.encode(s) for s in corpus] ) - 96 # [3, 5]
-    input_segs = torch.ones_like(input_seqs, dtype=torch.long)
-
-    # 右 PAD. PAD id --> 0
-    input_seqs = torch.cat([input_seqs, torch.zeros(3, 1, dtype=torch.long)], dim=1)
-    input_segs = torch.cat([input_segs, torch.zeros(3, 1, dtype=torch.long)], dim=1)
-
-    logits, _, _ = gpt2_model(input_seqs, input_segs=input_segs)
-
-    print(logits.shape)
-
-    # 测试 generate. eos_id 设定为 PAD id 0
-    prompt = ['aa', 'ab']
-    prompt_seqs = torch.tensor( [ chartok.encode(s) for s in prompt] ) - 96 # [B=2, S=2]
-    prompt_segs = torch.ones_like(prompt_seqs, dtype=torch.long)
-
-    max_gen_size = 4
-
-    output_ids = gpt2_model.generate(prompt_seqs, prompt_segs, max_gen_size, eos_id=0) + 96 # [B=2, max_gen_size=4]
-    output = []
-    for seq in output_ids.tolist():
-        output.append( chartok.decode(seq) )
-    
-    print(output)
+    for corpus in corpora_paths:
+        fname = os.path.basename(corpus)
+        save_path = os.path.join(save_dir, fname)
+        
+        with open(corpus, 'r', encoding='utf-8') as raw, open(save_path, 'w', encoding='utf-8') as tokenized:
+            text = raw.readline()
