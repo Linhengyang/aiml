65ce66318293afad8cd8f411090d35e2fcb757ba
linhengyang <linhengyang1@outlook.com>
2025-09-30 12:03:55 +0800

    update masked-cross-entropy-loss

--- src/core/loss/mask_ce_loss.py
+++ src/core/loss/mask_ce_loss.py
@@ -5,25 +5,7 @@ import torch.nn.functional as F
 
 class MaskedCrossEntropyLoss(nn.CrossEntropyLoss):
     '''
-    About nn.CrossEntropyLoss
-    input:
-        1. pred, tensor of classification logits. batch_idx on the first dim, logit values on the second dim, following dims are position dims
-           shape: (batch_size, num_cls, [d1...dk] as position dims)
-        2. label, tensor of classification labels. Label values are int64 values(not one-hot)
-           shape: (batch_size, [d1...dk] as position dims)
-    output:
-        if reduction set to 'none':
-        None-reduction CE Loss tensor w/ shape (batch_size, [d1...dk] as position dims). element is loss value for single classification
-    
-    About MaskedCrossEntropyLoss
-    input:
-        1. & 2. are inheritting from nn.CrossEntropyLoss
-        3. mask for valid_area, 0-1/True-False tensor of valid area of pred & label. Only losses from valid area will be calculated.
-           shape: (batch_size, [d1...dk] as position dims), same as label
-    output:
-
-        sum on dims other than first dim. Output Loss tensor shape (batch_size,)
-        Only losses from valid area are summed for every sample.
+    mask-version of CrossEntropyLoss
     '''
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -32,30 +14,26 @@ class MaskedCrossEntropyLoss(nn.CrossEntropyLoss):
         '''
         input: (batch_size, num_cls, [d1...dk])float as logits
         target: (batch_size, [d1...dk])int64 as label
-        mask: (batch_size, [d1...dk])bool as mask. only count loss of true-area.
+        mask: (batch_size, [d1...dk])bool as mask. only loss of true-area of target will be calculated
         '''
-
+        # cross-entropy loss behavior:
         # pred(B, C, [d1...dk])float, label(B, [d1...dk])long --> unmasked_loss(B, [d1...dk])float
         # pred(B, C)float, label(B,)long --> unmasked_loss(B,)float
         # pred(C,)float, label(1,)long --> unmasked_loss(1,)float
-        unmasked_loss = F.cross_entropy(
-            input,
-            target,
-            weight=self.weight,
-            ignore_index=self.ignore_index,
-            reduction='none',
-            label_smoothing=self.label_smoothing,
-        )
+
         if mask is None:
-            _loss = unmasked_loss
+            ignore_index = self.ignore_index
+            labels = target
         else:
-            _loss = unmasked_loss.masked_fill((mask==False), 0.)
+            ignore_index = -100
+            labels = target.clone() # 防御性编程, 避免影响源数据
+            labels[~mask] = ignore_index
         
-        if self.reduction == 'none':
-            return _loss
-        elif self.reduction == 'mean':
-            return _loss.mean()
-        elif self.reduction == 'sum':
-            return _loss.sum()
-        else:
-            raise ValueError(f'wrong arg `reduction` {self.reduction}. must be one of `mean`/`sum`/`none`.')
+        return F.cross_entropy(
+            input,
+            labels,
+            weight = self.weight,
+            ignore_index = ignore_index,
+            reduction = self.reduction,
+            label_smoothing = self.label_smoothing,
+            )
