a1fbdd9a4a031749faeb0cedf4c8f6d23ddc315e
linhengyang <linhengyang1@outlook.com>
2025-10-13 17:30:34 +0800

    update

--- src/projs/gpt2/optimizer.py
+++ src/projs/gpt2/optimizer.py
@@ -1,1 +1,11 @@
 # Optimizer for GPT
+# AdamW
+
+import torch
+from torch.optim import Optimizer
+from typing import Iterable, Optional, Dict, Any, Union
+
+
+class AdamW(Optimizer):
+    def __init__(self, params, defaults):
+        super().__init__(params, defaults)
