c793c140d72d78d9389e832139e1256b6567c1d0
linhengyang <linhengyang1@outlook.com>
2025-10-17 18:53:40 +0800

    update

--- src/kits/huggingface/state_dict_adapt.py
+++ src/kits/huggingface/state_dict_adapt.py
@@ -1,1 +1,6 @@
 # state_dict_adapt 里修改 huggingface/transformers 库标准的 model state_dict(weights), 以适应并载入 custom network
+from collections import OrderedDict
+from torch import Tensor
+
+def gpt2_state_dict_adaptor(state_dict: OrderedDict[str, Tensor]) -> OrderedDict[str, Tensor]:
+    pass
--- test.py
+++ test.py
@@ -10,26 +10,42 @@ gpt2_resource_dir = '../../resource/llm/gpt/gpt2/'
 
 
 
-from workspace.aiml.src.kits.huggingface.tokenizer_adapt import gpt2Tokenizer
+from src.kits.huggingface.tokenizer_adapt import gpt2Tokenizer
 from src.projs.gpt2.network import gpt2, gpt2Config
 
 
 if __name__ == "__main__":
-    # 测试 tokenizer
-    tokenizer_path = os.path.join(gpt2_resource_dir, 'tokenizer.json')
+    ## 测试 tokenizer
+    # tokenizer_path = os.path.join(gpt2_resource_dir, 'tokenizer.json')
 
-    tok = gpt2Tokenizer()
-    tok.from_doc(tokenizer_path)
+    # tok = gpt2Tokenizer()
+    # tok.from_doc(tokenizer_path)
 
-    text = '! hello I am linhengyang<|endoftext|>haha Iam back again..'
-    encoded = tok.encode(text, allowed_special='all')
-    text_ = tok.decode(encoded)
-    print(encoded)
-    assert text_ == text
+    # text = '! hello I am linhengyang<|endoftext|>haha Iam back again..'
+    # encoded = tok.encode(text, allowed_special='all')
+    # text_ = tok.decode(encoded)
+    # print(encoded)
+    # assert text_ == text
 
     # 测试加载 模型(pure-torch)
     model_path = os.path.join(gpt2_resource_dir, 'gpt2.bin')
     net_state_dict = torch.load(model_path, map_location='cpu')
-    print(net_state_dict.keys())
+    print(type(net_state_dict))
+    # 删除 
 
-    config = gpt2Config()
+    config = gpt2Config(
+        embd_size = 768,
+        vocab_size = 50257, 
+        embd_p_drop = 0.1,
+        num_head = 12,
+        use_bias = True,
+        max_context_size = 1024,
+        attn_p_drop = 0.1,
+        resid_p_drop = 0.1,
+        use_cached_casual_mask = True,
+        use_rope = False,
+        num_block = 1
+        )
+    net_init = gpt2(config)
+    # print(net_init.state_dict().keys())
+    print(type(net_init.state_dict()))
