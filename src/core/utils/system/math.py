1efed3dc3314690aec676fd08ff9402ac0fc792c
linhengyang <linhengyang1@outlook.com>
2025-10-28 12:50:25 +0800

    update

--- src/core/utils/system/math.py
+++ src/core/utils/system/math.py
@@ -1,6 +1,7 @@
 # math.py
 import math
 import numpy as np
+import typing as t
 
 
 
@@ -29,3 +30,35 @@ def cosine_similarity(a:np.ndarray, b:np.ndarray) -> np.float64:
     cosine_sim = dot_prod / (norm_a*norm_b)
 
     return cosine_sim
+
+
+
+
+def check_monotonic(
+        seq: list,
+        mode:t.Literal['increase', 'decrease'],
+        strict:bool=True
+        ) -> bool:
+
+    if mode == 'increase' and strict:
+        for i in range(len(seq) - 1): # 0 至 len(seq) - 2
+            if seq[i] >= seq[i+1]: # 假如 存在 当前大于或等于 下一个，即破坏了 严格单调递增
+                return False
+        return True # 遍历 第 0 至 len(seq) - 2 个元素 与其后一个的比较，若都是 <，说明它是严格单调递增的
+    elif mode == 'increase':
+        for i in range(len(seq) - 1): # 0 至 len(seq) - 2
+            if seq[i] > seq[i+1]: # 假如 存在 当前大于 下一个，即破坏了 单调递增
+                return False
+        return True # 遍历 第 0 至 len(seq) - 2 个元素 与其后一个的比较，若都是 <=，说明它是单调递增的
+    elif mode == 'decrease' and strict:
+        for i in range(len(seq) - 1): # 0 至 len(seq) - 2
+            if seq[i] <= seq[i+1]: # 假如 存在 当前小于等于 下一个，即破坏了 严格单调递减
+                return False
+        return True # 遍历 第 0 至 len(seq) - 2 个元素 与其后一个的比较，若都是 >，说明它是严格单调递减的
+    elif mode == 'decrease':
+        for i in range(len(seq) - 1): # 0 至 len(seq) - 2
+            if seq[i] < seq[i+1]: # 假如 存在 当前小于下一个，即破坏了 单调递减
+                return False
+        return True # 遍历 第 0 至 len(seq) - 2 个元素 与其后一个的比较，若都是 >=，说明它是单调递减的
+    else:
+        raise ValueError(f'wrong mode for {mode}. must be increase/decrease')
