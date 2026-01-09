6be57f42029aa74c8f564959e99b4ceb1a6b6f46
linhengyang <linhengyang1@outlook.com>
2026-01-09 14:33:29 +0800

    updated tokenizer using parquet dataset exceeds boostTokenizer by 5 times. delete boostTokenizer/mpTokenzier

--- src/apps/bpe_build/_jobs.py
+++ src/apps/bpe_build/_jobs.py
@@ -4,48 +4,28 @@ import pyarrow.parquet as pq
 import pyarrow as pa
 import os
 import regex as re
-from ...core.utils.text.tokenizer import bufferBBPE_u16Tokenizer
+from ...core.utils.text.tokenizer import boostBBPETokenizer, bufferBBPE_u16Tokenizer, mpBBPETokenizer
 
-# configs = yaml.load(open('src/apps/bpe_build/configs.yaml', 'rb'), Loader=yaml.FullLoader)
+configs = yaml.load(open('src/apps/bpe_build/configs.yaml', 'rb'), Loader=yaml.FullLoader)
 
-################## directories ##################
-
-
-################## buffer/save in workspace/cache ##################
-# buffer_dir = os.path.join( configs['cache_dir'], configs['app_name'], 'buffer' )
-# tokenizer_save_dir = os.path.join( configs['artifact_dir'], configs['app_name'], 'tokenizer' )
-# vocab_cache_dir = os.path.join( configs['artifact_dir'], configs['app_name'], 'vocab' )
+################# directories ##################
 
 
-################## backup init to data/TinyStories/bytes ##################
-# backup_init_dir = configs['backup_init_dir']
+################# buffer/save in workspace/cache ##################
+buffer_dir = os.path.join( configs['cache_dir'], configs['app_name'], 'buffer' )
+tokenizer_save_dir = os.path.join( configs['artifact_dir'], configs['app_name'], 'tokenizer' )
+vocab_cache_dir = os.path.join( configs['artifact_dir'], configs['app_name'], 'vocab' )
 
 
-
-################## backup init to data/TinyStories/bytes ##################
-# num_merges = configs['num_merges']
-# buffer_size = configs['buffer_size']
-# save_tok_name = configs['save_tok_name']
+################# backup init to data/TinyStories/bytes ##################
+backup_init_dir = configs['backup_init_dir']
 
 
 
-
-
-
-
-
-
-# def bpe_prepare():
-#     print('insight on dataset TinyStories')
-#     train_pq = configs['train_pq']
-#     valid_pq = configs['valid_pq']
-
-#     for pq_file in [train_pq, valid_pq]:
-#         meta_data = pq.ParquetFile(pq_file).metadata
-#         print(f'parquet file {pq_file} info:\n{pq.ParquetFile(pq_file).metadata}')
-
-#         if meta_data.num_rows // buffer_size >= 200:
-#             raise RuntimeError(f'Num of Batches {meta_data.num_rows // buffer_size} exceeds 200.')
+################# backup init to data/TinyStories/bytes ##################
+num_merges = configs['num_merges']
+buffer_size = configs['buffer_size']
+save_tok_name = configs['save_tok_name']
 
 
 
@@ -55,35 +35,55 @@ from ...core.utils.text.tokenizer import bufferBBPE_u16Tokenizer
 
 
 
+def bpe_prepare():
+    print('insight on dataset TinyStories')
+    train_pq = configs['train_pq']
+    valid_pq = configs['valid_pq']
+
+    for pq_file in [train_pq, valid_pq]:
+        meta_data = pq.ParquetFile(pq_file).metadata
+        print(f'parquet file {pq_file} info:\n{pq.ParquetFile(pq_file).metadata}')
+
+        if meta_data.num_rows // buffer_size >= 200:
+            raise RuntimeError(f'Num of Batches {meta_data.num_rows // buffer_size} exceeds 200.')
 
 
-# def bpe_train():
-#     print('begin to run BPE on dataset TinyStories')
-#     train_pq = configs['train_pq']
-#     valid_pq = configs['valid_pq']
-#     for folder in [buffer_dir, tokenizer_save_dir, vocab_cache_dir]:
-#         os.makedirs(folder, exist_ok=True)
+
+
+
+
+
+
+
+
+
+def bpe_train():
+    print('begin to run BPE on dataset TinyStories')
+    train_pq = configs['train_pq']
+    valid_pq = configs['valid_pq']
+    for folder in [buffer_dir, tokenizer_save_dir, vocab_cache_dir]:
+        os.makedirs(folder, exist_ok=True)
         
-#     tok = mpBBPETokenizer(name=save_tok_name, buffer_dir=buffer_dir)
-#     corpora = [valid_pq, ]
-#     colnames = ['text']*len(corpora)
+    tok = mpBBPETokenizer(name=save_tok_name, buffer_dir=buffer_dir)
+    corpora = [valid_pq, train_pq]
+    colnames = ['text']*len(corpora)
 
-#     tok.train_bpe(num_merges,
-#                   corpora=corpora,
-#                   colnames=colnames,
-#                   backup_init_tokens_dir=None,
-#                 #   buffer_size=buffer_size,
-#                   keep_window=0,
-#                   verbose=True
-#                   )
+    tok.train_bpe(num_merges,
+                  corpora=corpora,
+                  colnames=colnames,
+                  backup_init_tokens_dir=None,
+                #   buffer_size=buffer_size,
+                  keep_window=0,
+                  verbose=True
+                  )
 
-#     # save tokenizer
-#     tok_fpath = os.path.join(tokenizer_save_dir, f'{tok.name}.tok')
-#     tok.save(tok_fpath)
-#     # view vocab
-#     tok.view(tmpsave_dir = vocab_cache_dir)
+    # save tokenizer
+    tok_fpath = os.path.join(tokenizer_save_dir, f'{tok.name}.tok')
+    tok.save(tok_fpath)
+    # view vocab
+    tok.view(tmpsave_dir = vocab_cache_dir)
     
-#     print('BPE ends')
+    print('BPE ends')
 
 
 
@@ -101,13 +101,14 @@ def bpe_continue(tok_path:str|None):
 
     # continue train 下, corpora必须显式地输入None
     tok.train_bpe(num_merges = 3,
+                #   corpora = None,
                   keep_window = 3,
                   verbose = True
                   )
 
     # rename and save the updated tokenizer
-    tok.name = 'uint16_test'
-    tok_fpath = os.path.join('../cache/temp/buffer', f'{tok.name}.tok')
+    tok.name = '3merges'
+    tok_fpath = os.path.join('../tmp/bpe_build/', f'{tok.name}.tok')
     tok.save(tok_fpath)
 
     # # view vocab
