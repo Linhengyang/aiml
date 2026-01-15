1ea829f4ad7064630cf8d60a9653a8688f9dcc90
linhengyang <linhengyang1@outlook.com>
2026-01-15 10:00:37 +0800

    use py-package arangement instead of bin. store .so in /bin but extension to /ext

---
+++ ext/bpeboost/__init__.py
@@ -0,0 +1,4 @@
+from pair_count_merge import initialize_process, count_u16pair_batch, merge_u16pair_batch, close_process
+
+
+__all__ = ['initialize_process', 'count_u16pair_batch', 'merge_u16pair_batch', 'close_process']
--- test.py
+++ test.py
@@ -5,8 +5,7 @@ import pyarrow.dataset as ds
 import regex as re
 import os
 import typing as t
-from src.core.utils.file.folder_op import clean_folder
-from src.core.utils.text.tokenizer import bufferBBPE_u16Tokenizer
+import sys
 
 valid_pq = '../../data/TinyStories/raw/validation.parquet'
 train_pq = '../../data/TinyStories/raw/train.parquet'
@@ -16,4 +15,4 @@ raw_pq_dir = '../../data/TinyStories/raw/'
 
 
 if __name__ == "__main__":
-    pass
+    print(sys.path)
