562eadeaa4f6333fb71a502ce39909ae558dc1de
linhengyang <linhengyang1@outlook.com>
2026-01-20 14:35:33 +0800

    add tls_sort_count_u16pair_core

--- src/lib/tokenizer/core_count_pair_mt.cpp
+++ src/lib/tokenizer/core_count_pair_mt.cpp
@@ -13,7 +13,7 @@ u16token_pair_counts_ptrs tls_dict_count_u16pair_core(
     uint32_t* keys,
     const size_t len,
     mempool& pool,
-    counter_tls* counter
+    counter_st* counter
 ) {
     for(size_t i = 0; i < len; ++i) {
         counter->increment(keys[i]);
@@ -35,5 +35,40 @@ u16token_pair_counts_ptrs tls_dict_count_u16pair_core(
     
     return u16token_pair_counts_ptrs{L_uniq, R_uniq, counts, size};
 }
+
+
+
+u16token_pair_counts_ptrs tls_sort_count_u16pair_core(
+    uint32_t* keys,
+    const size_t len,
+    mempool& pool
+) {
+    uint16_t* L_uniq = static_cast<uint16_t*>(pool.allocate(len*sizeof(uint16_t)));
+    uint16_t* R_uniq = static_cast<uint16_t*>(pool.allocate(len*sizeof(uint16_t)));
+    uint64_t* counts = static_cast<uint64_t*>(pool.allocate(len*sizeof(uint64_t)));
+
+    // 排序, 可并行
+    std::sort(keys, keys+len);
+    
+    // 线性遍历计数
+    uint32_t prev = keys[0]; uint64_t cnt = 1; size_t size = 0;
+    for (size_t i = 1; i < len; ++i) {
+        if (keys[i] == prev) {
+            ++cnt; }
+        else {
+            // flush(prev, cnt)
+            L_uniq[size] = static_cast<uint16_t>(prev >> 16);
+            R_uniq[size] = static_cast<uint16_t>(prev & 0xFFFF);
+            counts[size] = cnt; ++size;
+
+            prev = keys[i]; cnt = 1; }
+    }
+    L_uniq[size] = static_cast<uint16_t>(prev >> 16);
+    R_uniq[size] = static_cast<uint16_t>(prev & 0xFFFF);
+    counts[size] = cnt; ++size;
+    
+    return u16token_pair_counts_ptrs{L_uniq, R_uniq, counts, size};
+}
+
 
 } // end of extern C
