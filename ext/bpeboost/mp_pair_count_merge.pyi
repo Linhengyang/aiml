7d4fb0c56bafdf04ec34828d831502e8dfea64dd
linhengyang <linhengyang1@outlook.com>
2026-01-21 11:10:37 +0800

    update

--- ext/bpeboost/mp_pair_count_merge.pyi
+++ ext/bpeboost/mp_pair_count_merge.pyi
@@ -5,16 +5,16 @@ def initialize_process(block_size: int) -> None:
     ...
 
 def count_u16pair_batch(
-    tokens_offsets,
-    ):
+    tokens_offsets: tuple,
+    ) -> tuple:
     ...
 
 def merge_u16pair_batch(
-    tokens_offsets,
+    tokens_offsets: tuple,
     pair_L:np.uint16,
     pair_R:np.uint16,
     new_token:np.uint16
-    ):
+    ) -> tuple:
     ...
 
 def close_process() -> None:
