67937ee72fc28192fbb0b3b1d09a046ba872d80d
linhengyang <linhengyang1@outlook.com>
2026-01-27 00:39:42 +0800

    update

--- src/core/models/transformer/model_transformer.py
+++ src/core/models/transformer/model_transformer.py
@@ -1,3 +1,4 @@
+from src.core.architectures import EncoderDecoder
 from src.core.layers.position_encoding import TrigonoAbsPosEnc
 from src.core.blocks.bert import BERTEncoderBlock
 from src.core.blocks.transformer import TransformerDecoderBlock
@@ -46,14 +47,14 @@ class TransformerEncoder(nn.Module):
         src_embd = self.embd_drop( src_embd + self.pos_encoding(position_ids) ) # (batch_size, src_context_size, hidden_size)
 
         # attention mask(batch_size, src_context_size, src_context_size): True --> valid area, False --> need masked
-        # 没有因果自回归, 只有 invalid area(PAD) --> False, valid area(non-PAD) --> True
-        src_attn_mask = position_ids[None, :] < src_valid_lens[:, None] # (batch_size, src_context_size)
-        src_attn_mask = src_attn_mask.unsqueeze(-1) * src_attn_mask.unsqueeze(-2)
+        # 没有因果自回归, 只有 位置id大于 valid_lens 部分为 invalid area(PAD) --> False, 位置id小于等于 valid_lens 部分为 valid area(non-PAD) --> True
+        src_attn_arr = position_ids[None, :] <= src_valid_lens[:, None] # (batch_size, src_context_size)
+        src_attn_mask = src_attn_arr.unsqueeze(-1) * src_attn_arr.unsqueeze(-2)
 
         for blk in self.blks:
             src_embd = blk(src_embd, src_attn_mask)
 
-        return src_embd, src_attn_mask # (batch_size, src_context_size, hidden_size) & (batch_size, src_context_size, src_context_size)
+        return src_embd, src_attn_arr # (batch_size, src_context_size, hidden_size) & (batch_size, src_context_size)
     
 
 
@@ -98,106 +99,58 @@ class TransformerDecoder(nn.Module):
 
 
     def forward(self,
+                encoded_output:Tuple[torch.Tensor, torch.Tensor],                       # [B, src_ctx_size, hidden_size]/[B, src_ctx_size]
                 tgt: torch.Tensor,                                                      # [B, max_decoder_ctx_size]
-                tgt_valid_lens: torch.Tensor,                                           # [B, ]
-                encoded_output:Tuple[torch.Tensor, torch.Tensor],                       # [B, src_ctx_size, hidden_size]/[B, src_ctx_size, src_ctx_size]
-                past_kv: Optional[Tuple[Tuple[torch.Tensor, torch.Tensor], ...]] = None,
+                tgt_valid_lens: Optional[torch.Tensor] = None,                          # [B, ]
+                past_kv: Optional[Tuple[Tuple[torch.Tensor, torch.Tensor], ...]] = None,# tuple of (past_k[B, H, L_past, d], past_v[B, H, L_past, d])
                 if_cache_kv: bool = False
                 ):
         
         if self.training:
+            # train: tgt 是 [B, max_decoder_ctx_size], tgt_valid_lens 是 [B, ]
             positions = torch.arange(0, tgt.size(1), dtype=torch.int64, device=tgt.device) # [max_decoder_ctx_size, ]
+            tgt_attn_arr = positions[None, :] < tgt_valid_lens[:, None]
+            tgt_attn_mask = tgt_attn_arr.unsqueeze(-1) * tgt_attn_arr.unsqueeze(-2) #[B, max_decoder_ctx_size, max_decoder_ctx_size]
         elif past_kv is None:
+            # infer on <bos>: tgt 是[B, 1]的<bos>tensor, tgt_valid_lens 不需要输入
             positions = torch.tensor([0], dtype=torch.int64, device=tgt.device)
+            tgt_attn_mask = torch.ones((tgt.size(0), 1, 1), dtype=torch.bool, device=tgt.device) #[B, 1, 1]
         else:
-            positions = torch.tensor([0], dtype=torch.int64, device=tgt.device)
-
-
-
-
-
-
-
-
-
-class TransformerDecoder(Decoder):
-    def __init__(self, vocab_size, num_blk, num_heads, num_hiddens, dropout, ffn_num_hiddens, use_bias):
-        super().__init__()
-        self.num_hiddens = num_hiddens
-        self.embedding = nn.Embedding(vocab_size, num_hiddens)
-        self.pos_encoding = TrigonoAbsPosEnc(num_hiddens)
-        self.dropout = nn.Dropout(dropout)
-        self.blks = nn.Sequential()
-        for i in range(num_blk):
-            cur_blk = TransformerDecoderBlock(i, num_heads, num_hiddens, dropout, ffn_num_hiddens, use_bias)
-            self.blks.add_module("decblock"+str(i), cur_blk)
-        self.dense = nn.Linear(num_hiddens, vocab_size)
-
-
-    def init_state(self, enc_outputs):
-        # encoder returns encoded_src, src_valid_lens
-
-        # train: src_enc_info = (src_enc, src_valid_lens): [(batch_size, num_steps, d_dim), (batch_size,)]
-        # infer: src_enc_info = (src_enc, src_valid_lens): [(1, num_stepss, d_dim), (1,)]
-        src_enc_info = enc_outputs
-
-        return src_enc_info
-
-
-    def forward(self, tgt_dec_input, src_enc_info, KV_Caches=None):
-
-        tgt_dec_input_embd = self.embedding(tgt_dec_input) * math.sqrt(self.num_hiddens) 
-        # 使用 固定位置编码时, 避免位置编码的影响过大，所以放大input embeddings
-
-        # target position embedding
-        # 训练模式: input 的 timesteps 是从 0(bos) 到 num_steps-1
-        if self.training:
-            _, num_steps = tgt_dec_input.shape
-            position_ids = torch.arange(0, num_steps, dtype=torch.int64, device=tgt_dec_input.device) # (num_steps,)
-        # 推理模式: 对于第i次infer, input 的 timestep 就是 i-1, 而这个信息可以从 KV_Caches 的values 中的第二个维度(dim=1)得到
-        else:
-            position_ids = torch.tensor([ 0 if KV_Caches == {} else KV_Caches['0'].size(1) ],
-                                        dtype=torch.int64, device=tgt_dec_input.device) # (1,)
-
-        # input embeddings + position embedding
-        # shapes of train: (batch_size, num_steps, num_hiddens) +(broadcast) (num_steps, num_hiddens)
-        # shapes of infer: (1, 1, num_hiddens) +(broadcast) (1, num_hiddens)
-        tgt_query = self.dropout(tgt_dec_input_embd + self.pos_encoding(position_ids))
-
-        # Decoder Block 的输入 tgt_query, src_enc_info, KV_Caches
-        for blk in self.blks:
-            # 循环过程中, 单次 blk 执行, 更新了 该 blk 对应的 KV_Caches 的 block-ID: tensor 的 kv对
-            tgt_query, KV_Caches = blk(tgt_query, src_enc_info, KV_Caches)
+            L_past = past_kv[0][0].size(2)
+            # infer on last predicted token: tgt 是[B, 1], tgt_valid_lens 不需要输入
+            positions = torch.tensor([L_past], dtype=torch.int64, device=tgt.device)
+            tgt_attn_mask = torch.ones((tgt.size(0), 1, L_past), dtype=torch.bool, device=tgt.device) #[B, 1, L_past+1]
         
-        # 一次 infer forward 过程, KV_Caches 中的每个 key-value pair, 都被更新, 则 整个 KV_Caches 被更新
+        x = self.token_embedding(tgt)*math.sqrt(self.D) # (batch_size, max_decoder_ctx_size/1, hidden_size)
+        x = self.embd_drop( x + self.pos_encoding(positions) ) # (batch_size, max_decoder_ctx_size/1, hidden_size)
 
-        #train: output[0] shape: (batch_size, num_steps, vocab_size)tensor of logits,  timestep 从 1 到 num_steps;
-        #       output[1]: None
-        #infer: output[0] shape: (1, 1, vocab_size)tensor of logits, 对于第i次infer, timestep 是 i;
-        #       output[1]: dict of 
-        #                      keys as block_indices
-        #                      values as (1, i, d_dim) tensor, i 维 包含 timestep 0-i-1, 实际上就是 input KV_Caches 添加 timestep i-1 的 tgt_query
-        return self.dense(tgt_query), KV_Caches
+        new_past_kv = [] if if_cache_kv else None
+        for i, block in enumerate(self.blks):
+            kv_cache = past_kv[i] if past_kv is not None else None
+            # new_kv_cache 是 kv_cache concate k & v from x
+            x, new_kv_cache = block(x, encoded_output, kv_cache, if_cache_kv, tgt_attn_mask)
+            if if_cache_kv:
+                new_past_kv.append( new_kv_cache ) # new_kv_cache 是 torch.cat 得到的, 其内存使用是高效的
+        
+        logits = self.head_tok(x) # [B, max_decoder_ctx_size/1, vocab_size]
+        return logits, tuple(new_past_kv) if if_cache_kv else None
 
 
 
+
+#TODO
 class Transformer(EncoderDecoder):
-    ## 整体两种实现模式:
-    # 1是encoder和decoder一起训练, 但分开使用, 这样transformer本身只需考虑train batch input的场景, infer的循环放在pred函数里
-    # 2是encoder和decoder一起训练, 一起使用, 这样transformer要注意infer sample input的场景, infer的循环放在forward里. 即针对 net.training 的状态, 写不同的infer
     def __init__(self, encoder, decoder):
         super().__init__()
         self.encoder = encoder
         self.decoder = decoder
-    
-    # net_inputs_batch = (X_batch, Y_frontshift1_batch, X_valid_lens_batch),
-    def forward(self, src, tgt_frontshift1, src_valid_lens):
-        enc_outputs = self.encoder(src, src_valid_lens) # src_enc, src_valid_lens
-        enc_info = self.decoder.init_state(enc_outputs)
-  
-        #train: output[0] shape: (batch_size, num_steps, vocab_size) tensor of logits, output[2]: None
-        #infer: output[0] shape: (1, 1, vocab_size) tensor of logits, output[2]: dict of (1, cur_infer_step i, d_dim) tensor
-        return self.decoder(tgt_frontshift1, enc_info)
+
+    def forward(self, src, tgt, src_valid_lens, tgt_valid_lens, past_kv, if_cache_kv):
+        encoded_outputs = self.encoder(src, src_valid_lens)
+
+        return self.decoder(encoded_outputs, tgt, tgt_valid_lens, past_kv, if_cache_kv)
+
+
 
 
 
