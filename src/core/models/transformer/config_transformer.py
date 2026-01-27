7066a98975539ec667bdc5ed83d09c9e50b9aceb
linhengyang <linhengyang1@outlook.com>
2026-01-27 13:15:01 +0800

    transformerConfig add

--- src/core/models/transformer/config_transformer.py
+++ src/core/models/transformer/config_transformer.py
@@ -2,7 +2,22 @@ from dataclasses import dataclass
 
 @dataclass
 class transformerConfig:
-    pass
+    # embd params
+    vocab_size: int
+    hidden_size: int
+    num_heads: int
+    use_bias: bool
+    ffn_hidden_size: int
+    # dropout params
+    embd_p_drop: float
+    attn_p_drop: float
+    resid_p_drop: float
+    # encoder params
+    num_enc_blks: int
+    # decoder params
+    num_dec_blks: int
+    max_decoder_ctx_size: int
+    use_cached_causal_mask: bool
 
 
 
