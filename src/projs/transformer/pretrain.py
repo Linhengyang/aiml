8f00591edc8b49e6e2b5061d5d5ad188729852c2
linhengyang <linhengyang1@outlook.com>
2026-01-27 15:29:00 +0800

    update

--- src/projs/transformer/pretrain.py
+++ src/projs/transformer/pretrain.py
@@ -5,13 +5,11 @@ import torch
 class transformer_loss(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.loss = MaskedCrossEntropyLoss(*args, **kwargs)
+        self.loss = MaskedCrossEntropyLoss(reduction='none')
     
-    def forward(self, Y_hat, Y_label, Y_valid_lens):
-        # Y_hat, tensor of logits(batch_size, num_steps, vocab_size)
-        # Y_label_batch: (batch_size, num_steps)
-        # Y_valid_lens_batch: (batch_size,)
-
-        valid_area = torch.arange(Y_label.size(1), dtype=torch.int32, device=Y_valid_lens.device).unsqueeze(0) < Y_valid_lens.unsqueeze(1)
-
-        return self.loss(Y_hat.permute(0,2,1), Y_label, valid_area)
+    def forward(self, tgt_hat, tgt, tgt_valid_lens):
+        # from network: tgt_hat, tensor of logits(batch_size, context_size, vocab_size)
+        # from dataset/label: tgt(batch_size, context_size), tgt_valid_lens(batch_size, )
+        # label_mask: (batch_size, context_size)bool
+        label_mask = torch.arange(tgt.size(1), dtype=torch.int64, device=tgt_valid_lens.device).unsqueeze(0) < tgt_valid_lens.unsqueeze(1)
+        return self.loss(tgt_hat.transpose(1, 2), tgt, label_mask)
