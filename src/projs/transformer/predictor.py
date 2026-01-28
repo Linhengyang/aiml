417f8ff4c8e68681e33714d8dc35f4e45a236cac
linhengyang <linhengyang1@outlook.com>
2026-01-28 16:37:53 +0800

    update

--- src/projs/transformer/predictor.py
+++ src/projs/transformer/predictor.py
@@ -6,9 +6,7 @@ from operator import itemgetter
 import typing as t
 from src.core.interface.infra_easy import easyPredictor
 from src.core.evaluation.evaluate import bleu
-from src.utils.text.text_preprocess import preprocess_space
-from src.utils.text.string_segment import sentence_segment_greedy
-from src.core.data.assemble import truncate_pad
+from .dataset import tensorize_tokens, segment_seq2seqText, preprocess_space
 
 
 class sentenceTranslator(easyPredictor):
@@ -19,7 +17,7 @@ class sentenceTranslator(easyPredictor):
                  ):
         super().__init__()
         # load src/tgt language vocab
-        self._vocab = vocab, 
+        self._vocab = vocab
 
         # set device
         if device is not None and torch.cuda.is_available():
@@ -38,33 +36,31 @@ class sentenceTranslator(easyPredictor):
         self.bleu_k = bleu_k
         self.length_factor = length_factor # length_factor 越大, 输出越偏好长序列
 
-    def predict(self, src_sentence, need_lower=True, separate_puncs='!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'):
-        self.src_sentence = preprocess_space(src_sentence, need_lower, separate_puncs, normalize_whitespace=True)
-        source, _ = sentence_segment_greedy(
-            self.src_sentence,
-            self._vocab.to_glossary(),
-            UNK_token = self._vocab.to_subwords(self._vocab.unk),
-            flatten = True,
-            need_preprocess = False # 已经有预处理了
-            )
+    def predict(self,
+                src_sentences: t.List[str],
+                need_lower=True, 
+                separate_puncs='!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'):
+        # 凑批
+        sample_separator, srctgt_separator = '\n', '\t'
 
-        # 和训练相同的tokenize: output src_array:(1, context_size)int64, valid_lens:(1,)int32
-        src = self._vocab[ source ] + [ self._vocab['<eos>'] ]
-        src = truncate_pad(source, self.net.decoder.decoder_context_size, self._vocab['<pad>'])
-        src_ids = torch.tensor( src, dtype=torch.int64).unsqueeze(0) # (context_size,) --> (1, context_size)int64
-        src_valid_lens = (src_ids != self._vocab['<pad>']).type(torch.int64).sum(1) # (1,)int64
-        output_tokens = self.net.generate(src_ids, src_valid_lens, self.max_gen_size, self._vocab['<bos>'], self._vocab['<eos>'], self.temporature, self.topk)
-        
+        raw_text = sample_separator.join([src + srctgt_separator + " " for src in src_sentences])
+        source, _, _ = segment_seq2seqText(raw_text, sample_separator, srctgt_separator, self._vocab.to_glossary(),
+                                           self._vocab.to_subwords(self._vocab.unk), need_lower, separate_puncs)
+        src, src_valid_lens = tensorize_tokens(source, self._vocab, self.max_gen_size) #[B, max_gen_size=context_size], [B,]
+
+        # output_tokens: [B, gen_size]
+        output_ids = self.net.generate(src, src_valid_lens, self.max_gen_size, self._vocab['<bos>'], self._vocab['<eos>'], self.temporature, self.topk)
+        sentences = self._vocab.to_subwords(output_ids.tolist()) # map to list of list of subwords
         eow = self._vocab.to_subwords(self._vocab.eow)
         if eow: # 如果 eow 不是空字符
-            self.pred_sentence = re.sub(eow, ' ', ''.join(output_tokens))
+            self.pred_sentences = [re.sub(eow, ' ', ''.join(subwords)) for subwords in sentences]
         else: # 如果 eow 是空字符
-            self.pred_sentence = ' '.join(output_tokens)
+            self.pred_sentences = [' '.join(subwords) for subwords in sentences]
         
-        return self.pred_sentence
+        return self.pred_sentences
 
-    def evaluate(self, tgt_sentence, need_lower=True, separate_puncs='!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'):
-        assert hasattr(self, 'pred_sentence'), f'predicted target sentence not found'
-        self.tgt_sentence = preprocess_space(tgt_sentence, need_lower, separate_puncs, normalize_whitespace=True)
+    def evaluate(self, tgt_sentences, need_lower=True, separate_puncs='!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'):
+        assert hasattr(self, 'pred_sentences'), f'predicted target sentence not found'
+        self.tgt_sentences = [preprocess_space(sentence, need_lower, separate_puncs, normalize_whitespace=True) for sentence in tgt_sentences]
         
-        return self.eval_fn(self.pred_sentence, self.tgt_sentence, self.bleu_k)
+        return [self.eval_fn(pred, tgt, self.bleu_k) for pred, tgt in zip(self.pred_sentences, self.tgt_sentences)]
