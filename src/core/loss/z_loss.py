f70b153d7883a4b01980c0f0d5fdcb33cb709a9a
linhengyang <linhengyang1@outlook.com>
2026-03-03 22:01:56 +0800

    bug fix

--- src/core/loss/z_loss.py
+++ src/core/loss/z_loss.py
@@ -9,7 +9,7 @@ import torch
 
 class ZLoss(_Loss):
 
-    __constants__ = ["reduction"]
+    __constants__ = ["alpha", "reduction"]
 
     def __init__(self, alpha: float, size_average=None, reduce=None, reduction: str = "mean") -> None:
         super().__init__(size_average, reduce, reduction)
