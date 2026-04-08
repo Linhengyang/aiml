d329e8defa01f0e3e4462cb333c4517d7b192555
linhengyang <linhengyang1@outlook.com>
2026-04-08 20:17:25 +0800

    bpe in py

--- src/utils/text/tokenizer.py
+++ src/utils/text/tokenizer.py
@@ -1648,70 +1648,125 @@ class BBPETokenizer(baseBBPETokenizer):
         unique_words = ['abcbc', 'abcabc', 'bcdbca']
         unique_words = [list( map(ord, word) ) for word in unique_words]
         counts = [3, 5, 2]
-        BoW = (unique_words, counts)
 
-        # pair_counts <-- dict of pairs-counts: (l_tok, r_tok): cnts_of_key_pair 
-        # where_to_update <-- dict of pairs-positions: (l_tok, r_tok): postions_of_key_pair
-        pair_counts = {}
-        where_to_update = {}
-        for iw, word in enumerate(unique_words):
+        # pair_counts <-- dict of pairs-counts: (l_tok, r_tok): int of cnts_of_key_pair 
+        # where_to_update <-- dict of pairs-positions: (l_tok, r_tok): set of postions_of_key_pair
+        pair_counts = defaultdict(int)
+        where_to_update = defaultdict(set)
+
+        for pos, word in enumerate(unique_words):
             for l_tok, r_tok in zip(word[:-1], word[1:]):
-                if (l_tok, r_tok) not in pair_counts:
-                    pair_counts[(l_tok, r_tok)] = counts[iw]
-                else:
-                    pair_counts[(l_tok, r_tok)] += counts[iw]
-
-                if (l_tok, r_tok) not in where_to_update:
-                    where_to_update[(l_tok, r_tok)] = set([iw])
-                else:
-                    where_to_update[(l_tok, r_tok)].add(iw)
+                # defaultdict of int: 当 (l_tok, r_tok) 不存在时, 插入该 key 并设置 default value 为 0 / set()
+                pair_counts[(l_tok, r_tok)] += counts[pos]
+                where_to_update[(l_tok, r_tok)].add(pos)
         
         # heap: maxHeap to always pop out token-pairs with max counts along with its positions
         max_heap = []
-
         for tok_pair, positions in where_to_update.items():
             # node: (tokens_pair, counts_pair, positions_pair)
             to_merge_node = (tok_pair, pair_counts[tok_pair], positions)
             heapq.heappush(max_heap, (-to_merge_node[1], to_merge_node))
         
+        # empty the where_to_update, waiting new emerged pairs to insert
+        where_to_update = defaultdict(set)
 
         # merge_loop
         merge_cnts = 0
         while merge_cnts < num_merges:
-            _, ((l_tok, r_tok), counts, positions) = heapq.heappop(max_heap)
+            try:
+                _, (tok_pair, p_counts, positions) = heapq.heappop(max_heap)
+            except IndexError:
+                break
 
-            if counts != pair_counts[(l_tok, r_tok)]:
+            if p_counts != pair_counts[tok_pair]:
                 to_merge_node = (tok_pair, pair_counts[tok_pair], positions)
                 heapq.heappush(max_heap, (-to_merge_node[1], to_merge_node))
                 continue
+            
+            if p_counts < 1:
+                break
 
-            # merge
+            # BPE begin: we got tok_pair(l_tok, r_tok), positions(indices of unique words to execute merge)
             new_tok = 256 + merge_cnts
+            l_tok, r_tok = tok_pair
+            print(f'merge {(l_tok, r_tok)} to {new_tok}: occurence {p_counts}')
 
-            # changes: records of new generated/vanished pairs triggered by mergeing l_tok, r_tok along with positions
-            generated = defaultdict() # dict of position: generated pairs pos: [(l_tok, r_tok), ]
-            vanished = defaultdict() # dict of position: vanished pairs pos: [(l_tok, r_tok), ]
             for pos in positions:
-                # generate changes for unique_words at index pos
-                if pos not in generated:
-                    generated[pos] = []
-                if pos not in vanished:
-                    vanished[pos] = []
 
+                # changes: records of emerged(+1)/vanished(-1) pairs triggered by mergeing l_tok, r_tok among this word at unique_words[pos]
+                changes = [] # list of pair-signal: [(l_tok, r_tok), +-1]
+
+                # merge specific word, e.g:
                 # abcbc -> aXbc -> aXX
                 # abcabc -> aXabc -> aXaX
                 # bcdbca -> Xdbca -> XdXa
-                word = unique_words[pos]
-                length = len(word)
-
-                for i in range(length-1):
-                    # index i & i+1 match l_tok, r_tok
+                word = unique_words[pos] # in-place update word
+                i = 0
+                while True:
+                    # escape the loop when i reaches the last token
+                    if i >= len(word)-1:
+                        break
+                    # i+1 still valid
+                    # pair-found! when pair index i & i+1 match l_tok, r_tok.
                     if word[i] == l_tok and word[i+1] == r_tok:
-                        # vanished 1: the matched pair
-                        vanished[pos].append((l_tok, r_tok))
-                        # vanished 2: if there is a previous token prev_tok, then pair (prev_tok, l_tok) vanished
+                        # record the changes except the merging pair l_tok, r_tok: it was poped out from heap
+                        # since the merging pair will no longer be used/encountered, no need to update its pair-counts to 0 --> no need to record in changes
+                        # if prev_tok exists, pair(prev_tok, l_tok) vanished 1 times, pair(prev_tok, new_tok) emerged 1 times
                         if i > 0:
-                            vanished[pos].append((word[i-1], l_tok))
-                        # vanished 3: if there is following token fol_tok, then pair (r_tok, fol_tok) vanished
-                        if i+1 < length-1:
-                            vanished[pos].append((r_tok, word[i+2]))
+                            changes.append( ((word[i-1], l_tok),  -1) )
+                            # this will be updated to where_to_update: the merging candidates. if there are pre-requists for merging candidates, filter here
+                            changes.append( ((word[i-1], new_tok), 1) )
+                        # if fllw_tok exists, pair(r_tok, fllw_tok) vanished 1 times, pair(new_tok, fllw_tok) emerged 1 times
+                        if i+1 < len(word)-1:
+                            changes.append( ((r_tok, word[i+2]),  -1) )
+                            # this will be updated to where_to_update: the merging candidates. if there are pre-requists for merging candidates, filter here
+                            changes.append( ((new_tok, word[i+2]), 1) )
+                        word[i] = new_tok
+                        word.pop(i+1)
+                    i += 1
+                
+                # apply the changes of this position to pair_counts & where_to_update
+                for pair, change in changes:
+                    # update the counts of pair by change * cnts of the word
+                    pair_counts[pair] += change*counts[pos]
+                    # add the emerged pairs to where_to_udpate
+                    if change > 0:
+                        where_to_update[pair].add(pos)
+            
+            # use where_to_update to update heap
+            for tok_pair, positions in where_to_update.items():
+                # node: (tokens_pair, counts_pair, positions_pair)
+                to_merge_node = (tok_pair, pair_counts[tok_pair], positions)
+                heapq.heappush(max_heap, (-to_merge_node[1], to_merge_node))
+
+            merge_cnts += 1
+            where_to_update = defaultdict(set)
+
+
+
+
+
+
+
+# 从上述的例子中可以看出, 在对单个 word 执行 merge(l_tok, r_tok, new_tok) 的过程中, 原生 word 必须被修改
+# 论述: 当单个 word 中存在多个 pair(l_tok, r_tok) 时, 定位这些 pair 的位置(l_tok的index) 分别为 j_1, j_2, .., j_p 等
+# 定义 pair_n 的邻域: prev(if any), (l_tok, r_tok)->new_tok, fllw(if any)
+# 当 p = 1 时, 显然 changes 只和 pair_1 的邻域相关
+# 当 任意 j_k - j_q > 2 时, 即任意 2个 pair(l_tok, r_tok) 之间间隔了其他tok, 显然任意 pair_j 在merge时产生的 changes 只与 pair_j 的邻域有关
+# ---> pair_1, ..., pair_p 之间具备 local 对称性: 即执行 pair-merge 的顺序不影响 changes 的结果
+# ---> word 不需要根据 pair-merge 实时改变
+
+# 当 存在 j_k - j_q = 2 时, 即存在 至少 2个连续的 pair(l_tok, r_tok) pair_k, pair_q, .., 时, 其中一个 pair 的merge会改变另一个相邻pair的邻域
+# ---> pair_1, ..., pair_p 之间不具备 local 对称性: 即执行 pair-merge 的顺序影响 changes 的结果
+# ---> pair_1的merge, 改变了 pair_2 的左邻域:
+#      (l_tok_1, r_tok_1), (l_tok_2, r_tok_2) --> new_tok_1, (l_tok_2, r_tok_2)
+#      这里(r_tok_1, l_tok_2)vanish被正确记录于changes, (new_tok_1, l_tok_2)emerge被错误记录于changes
+#      new_tok_1(l_tok_2, r_tok_2) --> new_tok_1, new_tok_2
+#      这里(new_tok_1, l_tok_2)vanish被错误记录于changes, (new_tok_1, new_tok_2)emerge被正确记录于changes
+
+#      (new_tok_1, l_tok_2)分别出现了一次错误emerge和错误vanish, 所以其在 pair_counts 中的统计被 reduce 抵消 导致 正确
+#      (new_tok_1, l_tok_2)出现的一次错误emerge记录会拿来更新 where_to_update，导致这个 pair 在未来从heap中pop出时, 要多扫描一些噪声word位置
+#      也可以在每个 word 产生的 changes 作根据 pair 的reduce sum, 以得到 该word真正正确的 pair-emerge 记录(emerge>vanish)
+# ---> word 需要根据 pair-merge 实时改变：前一个pair-merge的后邻域的emerge 抵消于 后一个pair-merge的前邻域的vanish
+
+# 总结 --> 定位多个 pair(l_tok, r_tok)之后, 根据扫描顺序依次执行 pair-merge, 执行merge后 实时 更新word, 使得下一个pair-merge执行在更新后的word上
